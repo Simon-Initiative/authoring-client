03cd704eb22948be6a53cf5d10bdcac24968e1d4
vvelsen <vvelsen@gmail.com>
2017-03-03 12:15:33 -0500

    First attempt and adding the main container frame that will direct the global flow of OLI interaction, at least for an instructor or course developer.

--- src/Main.tsx
+++ src/Main.tsx
@@ -71,15 +71,33 @@ class Main extends React.Component<MainProps, {}> {
     }
   }
 
-  render(): JSX.Element {
-
+  render(): JSX.Element 
+  {
     let modalDisplay = this.props.modal !== null ? <div>{this.props.modal}</div> : <div></div>;
     
     return (
-      <div>
-        {modalDisplay}
-        <NavigationBar documentActions={this.documentActions}/>
-        {this.getView(this.props.document)}
+      <div style={{width: "inherit", height: "inherit"}}>
+        {modalDisplay}        
+		<div style={{"display": "flex", flexDirection: 'column', justifyContent: 'space-between', alignItems: 'stretch',  alignContent: 'stretch', height: "inherit"}}>
+		
+			<div style={{flex: "none", flexGrow: 0, order: 0, background: "#f1f1f1", border: "0px solid #c4c0c0", width: "100%", height: "32px", padding: "0px", margin: "0 0 0 0"}}>
+				<img src="assets/oli-icon.png" style={{float: "left", border: "0px solid #c4c0c0", width: "32px", height: "32px", padding: "0px", margin: "0px"}} />
+				<div style={{float: "left", margin: "0px", marginTop: "8px", fontSize: "12pt", fontWeight: "bold"}}>Welcome to OLI</div>
+			</div>				
+			
+			<div style={{"display": "flex", flexGrow: 1, order: 1, margin: "0 0 4px 0", flex: 1}}>
+				<div style={{"border-right" : "1px solid grey"}}>
+    				<NavigationBar documentActions={this.documentActions}/>
+				</div>
+				<div style={{background: "#f1f1f1", border: "1px solid #c4c0c0", padding: "2px", margin: "2px 2px 2px 2px", flex: 1}}>
+				{this.getView(this.props.document)}
+				</div>
+			</div>
+			
+			<div style={{"display": "flex", flexGrow: 0, order: 2, background: "#f1f1f1", border: "0px solid #c4c0c0", width: "100%", height: "30px", margin: "0 0 4px 0"}}>
+			Statusbar goes here if we want one
+			</div>						
+		</div>
       </div>
     )
   }
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,32 +1,43 @@
+
 'use strict'
 
 import * as React from 'react';
-
 import { document } from '../actions/document';
 
-interface NavigationBar {
+/**
+*
+*/
+interface NavigationBar 
+{
   
 }
 
-export interface NavigationBarProps {
+/**
+*
+*/
+export interface NavigationBarProps 
+{
   documentActions: any;
 }
 
-class NavigationBar extends React.PureComponent<NavigationBarProps, {}> {
-
-  render() {
+/**
+*
+*/
+class NavigationBar extends React.PureComponent<NavigationBarProps, {}> 
+{
+  render() 
+  {
     return (
-      <header className="navbar">
-        <section className="navbar-section">
-            <a onClick={this.props.documentActions.viewAllCourses} 
-              className="btn btn-link">My Courses</a>
-        </section>
-        
-      </header>);
+    		<ul className="unstyled">
+    			<li><a onClick={this.props.documentActions.viewAllCourses} className="btn btn-link">My Courses</a></li>
+    			<li><a className="btn btn-link">Outline Editor</a></li>
+    			<li><a className="btn btn-link">Learning Objectives</a></li>
+    			<li><a className="btn btn-link">Activity Editor</a></li>
+    			<li><a className="btn btn-link">Asset Manager</a></li>
+    			<li><a className="btn btn-link">Analytics</a></li>
+			</ul>			
+      );
   }
-
 }
 
 export default NavigationBar;
-
-
