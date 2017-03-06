c201e8eba07a0962783333615e7968d868a7e56f
Darren Siegel <darrensiegel@cmu.edu>
2017-03-06 12:41:02 -0500

    Add documentation

--- src/editors/manager/EditorServices.ts
+++ src/editors/manager/EditorServices.ts
@@ -3,6 +3,14 @@ import * as types from '../../data/types';
 import {modalActions} from '../../actions/modal';
 import {document as documentActions} from '../../actions/document';
 
+/**
+ * An interface that defines the  'services' that are available to 
+ * an editor.  'Services' can be thought of as any application level
+ * function or facility. Largely this abstraction exists to allow
+ * us to define document and content editors that are completely unaware
+ * of the Redux dispatcher.  The service implementation (see below)
+ * can effectively hide the presence and invocation of dispatch.  
+ */
 export interface EditorServices {
 
   // Request to view a document with the specified document id.
