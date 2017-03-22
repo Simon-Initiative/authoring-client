458c1fe697aa5bd5418e458ecddc36ab8b0e5f60
vvelsen <vvelsen@gmail.com>
2017-03-22 13:22:52 -0400

    Added more meat to the organization panel. Added as much bootstrap as I can figure out how to add. Will probably need to adjusted.

--- src/components/CreateOrganizationView.tsx
+++ src/components/CreateOrganizationView.tsx
@@ -24,6 +24,14 @@ export interface CreateOrganizationViewProps
   userId: string;    
 }
 
+const tempnavstyle=
+{
+    h2:
+    {
+        marginRight: '10px'
+    }
+};
+
 /**
 *
 */
@@ -35,11 +43,17 @@ class CreateOrganizationView extends React.PureComponent<CreateOrganizationViewP
     this.state = {
                     treeData: [
                                 {
-                                    title: 'Chicken',
+                                    title: 'Logic',
                                     children: [ 
-                                                { title: 'Egg' } 
+                                                { title: 'Pre Test' } 
                                               ] 
-                                }
+                                },
+                                {
+                                    title: 'Sets',
+                                    children: [ 
+                                                { title: 'Methods for Prevention' } 
+                                              ] 
+                                }    
                               ]
                  };
     this.services = new DispatchBasedServices(this.props.dispatch);
@@ -52,6 +66,12 @@ class CreateOrganizationView extends React.PureComponent<CreateOrganizationViewP
         <div className="row">
             <NavigationBar viewActions={this.viewActions} />
             <div className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
+                <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                    <p className="h2" style={tempnavstyle.h2}>Course Content</p>
+                    <button type="button" className="btn btn-secondary">Add Item</button>
+                    <a className="nav-link" href="#">+ Expand All</a>
+                    <a className="nav-link" href="#">- Collapse All</a>
+                </nav>
                 <SortableTree
                     treeData={this.state.treeData}
                     onChange={treeData => this.setState({ treeData })}
