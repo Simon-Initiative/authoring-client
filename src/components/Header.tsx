eeb0a73e9fad79ce51596c3517afddf221cc9766
Darren Siegel <siegel.darren@gmail.com>
2017-03-24 11:32:50 -0400

    links in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import * as viewActions from '../actions/view';
 
 interface Header {  
+  _onClickHome: () => void;
+  _onClickCreate: () => void;
 }
 
 export interface HeaderProps {
@@ -11,6 +14,17 @@ class Header extends React.PureComponent<HeaderProps, {}> {
 
   constructor(props) {
     super(props);
+
+    this._onClickCreate = this.onClickCreate.bind(this);
+    this._onClickHome = this.onClickHome.bind(this);
+  }
+
+  onClickHome() {
+    this.props.dispatch(viewActions.viewAllCourses());
+  }
+
+  onClickCreate() {
+    this.props.dispatch(viewActions.viewCreateCourse());
   }
 
   render() {
@@ -19,7 +33,7 @@ class Header extends React.PureComponent<HeaderProps, {}> {
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" onClick={this._onClickHome}>
           <img src="assets/oli-icon.png" width="30" height="30" className="d-inline-block align-top" alt=""/>
           Open Learning Initiative
         </a>
@@ -29,7 +43,7 @@ class Header extends React.PureComponent<HeaderProps, {}> {
               <a className="nav-link active" href="#">Register <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Create a Course</a>
+              <a className="nav-link" onClick={this._onClickCreate}>Create a Course</a>
             </li>
           </ul>
         </div>
