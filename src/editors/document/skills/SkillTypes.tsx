84e8118afc5a8a64e99f0726f57c670550d755cf
vvelsen <vvelsen@gmail.com>
2017-04-11 13:24:34 -0400

    Refined the skill model a little so that we have an opportunity to swap in and out different types of skill tracking. Currently we include two types.

--- src/editors/document/skills/SkillTypes.tsx
+++ src/editors/document/skills/SkillTypes.tsx
@@ -9,18 +9,114 @@ export const SkillTypes = types.strEnum([
 
 export type SkillTypes = keyof typeof SkillTypes;
 
-export class Skill {
-  orgType:SkillTypes=SkillTypes.Skill;
-      
-  title:string="unassigned";
-  id:string=guid();    
+export abstract class SkillModel {
+    sType:string="Base";
+    
+  /**
+   * 
+   */  
+  abstract toJSONObject (): Object;
+    
+  /**
+   *
+   */
+  abstract increment (aValue:number): void;
+     
+  /**
+   *
+   */
+  abstract decrement (aValue:number): void;
+}
+
+export class SkillModelBKT extends SkillModel {
+    
+  constructor () 
+  {
+    super ();
+    this.sType="BKT";      
+  }  
     
   // BKT probabilities
   pGuess:number=0.1;
   pKnown:number=0.25;
   pSlip:number=0.1;
   pMastery:number=0.95;
-        
+    
+  /**
+   * 
+   */  
+  toJSONObject (): Object {
+    var ephemeral:Object=new Object ();
+
+    ephemeral ["type"]=this.sType;  
+    ephemeral ["@pGuess"]=this.pGuess;
+    ephemeral ["@pKnown"]=this.pKnown;
+    ephemeral ["@pSlip"]=this.pSlip;
+    ephemeral ["@pMastery"]=this.pMastery;
+            
+    return (ephemeral);
+  }
+    
+  /**
+   *
+   */
+  increment (aValue:number) {
+  }
+    
+  /**
+   *
+   */
+  decrement (aValue:number) {
+  }    
+}
+
+export class SkillModelOLI extends SkillModel {
+
+  constructor () {
+    super ();
+    this.sType="OLI";      
+  }    
+  // OLI skill probabilities
+  prob:number=0.70;
+  gamma0:number=0.70;
+  gamma1:number=0.70;
+  lambda0:number=1.00;
+    
+  /**
+   * 
+   */  
+  toJSONObject (): Object {
+    var ephemeral:Object=new Object ();
+
+    ephemeral ["type"]=this.sType;      
+    ephemeral ["@p"]=this.prob;
+    ephemeral ["@gamma0"]=this.gamma0;
+    ephemeral ["@gamma1"]=this.gamma1;
+    ephemeral ["@lambda0"]=this.lambda0;
+            
+    return (ephemeral);
+  }
+    
+  /**
+   *
+   */
+  increment (aValue:number) {
+  }
+    
+  /**
+   *
+   */
+  decrement (aValue:number) {
+  }    
+}    
+
+export class Skill {
+  orgType:SkillTypes=SkillTypes.Skill;
+      
+  title:string="unassigned";
+  id:string=guid();    
+  skillModel:SkillModel=new SkillModelBKT ();  
+              
   constructor() {
 
   }
@@ -33,11 +129,22 @@ export class Skill {
       
     ephemeral ["@id"]=this.id;
     ephemeral ["@title"]=this.title;
-    ephemeral ["@pGuess"]=this.pGuess;
-    ephemeral ["@pKnown"]=this.pKnown;
-    ephemeral ["@pSlip"]=this.pSlip;
-    ephemeral ["@pMastery"]=this.pMastery;
+    ephemeral ["#skillModel"]=this.skillModel.toJSONObject ();
             
     return (ephemeral);
   }  
+    
+  /**
+   *
+   */
+  increment (aValue:number) {
+      this.skillModel.increment (aValue);
+  }
+    
+  /**
+   *
+   */    
+  decrement (aValue:number) {
+      this.skillModel.decrement (aValue);
+  }    
 }
