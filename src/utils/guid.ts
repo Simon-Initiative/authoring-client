c017896af498822f047ebe79bceb9291c3865bbe
Darren Siegel <siegel.darren@gmail.com>
2017-05-16 08:04:51 -0400

    add tslint

--- src/utils/guid.ts
+++ src/utils/guid.ts
@@ -2,11 +2,12 @@
 /**
  * Returns an RFC4122 version 4 compliant GUID.
  * See http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
- * for source and related discussion. 
+ * for source and related discussion. d
  */
 export default function guid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.random() * 16 | 0; 
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
