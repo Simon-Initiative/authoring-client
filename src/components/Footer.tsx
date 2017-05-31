f35363e07a910364edb323734f54022f71b3363f
nleaf <nickleafs@gmail.com>
2017-05-31 15:42:40 -0400

    Style updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,9 +19,9 @@ class Footer extends React.PureComponent<FooterProps, {}> {
         <div className="container">
             <div className="row">
                 <div className="license col-md-8">
-                  <img src="https://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" className="ccLicense" />
+                  <img src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" className="ccLicense" />
                   Unless otherwise noted this work is licensed under a 
-                    <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" rel="license"> Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>
+                    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" rel="license"> Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License</a>
                 </div>
                 <div className="col-md-3" role="contentinfo" id="tinyfooter">
                     <a className="oli" href="/">Open Learning Initiative</a>
--- src/editors/document/course/CourseEditor.tsx
+++ src/editors/document/course/CourseEditor.tsx
@@ -1,14 +1,16 @@
 'use strict'
 
 import * as React from "react";
+import * as persistence from "../../../data/persistence";
 //import {CourseResource} from "../common/resources";
 import * as models from "../../../data/models";
+import * as courseActions from '../../../actions/course';
 
 import {AbstractEditor, AbstractEditorProps, AbstractEditorState} from "../common/AbstractEditor";
 import {Resource} from "../../../data/resource";
 
 interface CourseEditor {
-
+    _registration: (developers: string, action: string) => any;
 }
 
 export interface CourseEditorProps extends AbstractEditorProps<models.CourseModel> {
@@ -24,7 +26,39 @@ class CourseEditor extends AbstractEditor<models.CourseModel, CourseEditorProps,
 
     constructor(props) {
         super(props, {resources: []});
+        this._registration = this.registration.bind(this);
     }
+
+    registration(developers: string, action: string) {
+        const courseId = this.props.model.guid;
+        persistence.developerRegistration(courseId, [developers], action)
+          .then(result => this.refreshCoursePackage(courseId))
+          .catch(err => console.log(err));
+      }
+
+     refreshCoursePackage(courseId: string) {
+        persistence.retrieveCoursePackage(courseId)
+          .then((document) => {
+            // Get an updated course content package payload
+            // if (document.model.modelType === models.ModelTypes.CourseModel) {
+            //   this.props.dispatch(courseActions.courseChanged(document.model));
+            // }
+          })
+          .catch(err => console.log(err));
+      }
+    // registerUser(courseId: string, userNames: string[], action: string) {
+    //     console.log("registerUser (" + courseId + ")");
+    //     persistence.developerRegistration(courseId, userNames, action)
+    //       .then(document => {
+    //         // Notify that the course has changed when a user views a course
+    //         if (document.model.modelType === models.ModelTypes.CourseModel) {
+    //           this.props.dispatch(courseActions.courseChanged(document.model));
+    //           this.props.dispatch(viewActions.viewDocument(courseId));
+    //         }
+
+    //       })
+    //       .catch(err => console.log(err));
+    // }
 
     componentDidMount() {
         // Fetch the titles of all current course resources
@@ -81,26 +115,28 @@ class CourseEditor extends AbstractEditor<models.CourseModel, CourseEditorProps,
         //       <td>{r.type}</td>
         //       <td>{link(r._id, r.title)}</td>
         //   </tr>)
-        let rows = this.props.model.resources.map(r =>
-            <tr key={r.guid}>
-                <td>{r.type}</td>
-                <td>{r.title}</td>
-            </tr>)
+        let developers = this.props.model.developers.map(d =>
+            <div className="row user">
+                <div className="col-10">
+                    <span className="profile"></span>
+                    <span className="name">{d.firstName} {d.lastName}</span>
+                    <span className="inst">Carnegie Mellon University</span>
+                    <span className="email">{d.email}</span>
+                </div>
+                <div className="col-2">
+                    <button type="button" className="btn btn-success" onClick={this._registration(d.userName, "remove")}>Remove</button>
+                </div>
+            </div>
+            )
         return (
-            <div className="row">
-                <h2>All Resources</h2>
-                <table className="table table-striped table-hover">
-                    <thead>
-                    <tr>
-                        <th>Type</th>
-                        <th>Title</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {rows}
-                    </tbody>
-                </table>
-            </div>);
+            <div className="row users">
+                    <div className="col-md-9">
+                        <h2>Team Members</h2>
+                        <div className="userContain">
+                            {developers}
+                        </div>
+                    </div>
+                </div>);
     }
 
     renderWebContent() {
@@ -199,7 +235,8 @@ class CourseEditor extends AbstractEditor<models.CourseModel, CourseEditorProps,
 
                 </div>
                 <br/><br/>
-                <div className="row users">
+                {this.renderResources()}
+                {/*<div className="row users">
                     <div className="col-md-9">
                         <h2>Team Members</h2>
                         <div className="userContain">
@@ -228,7 +265,7 @@ class CourseEditor extends AbstractEditor<models.CourseModel, CourseEditorProps,
 
                         </div>
                     </div>
-                </div>
+                </div>*/}
                 {/* 
                 <div className="row">
                     <div className="col-md-9">
