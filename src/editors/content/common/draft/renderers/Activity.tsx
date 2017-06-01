32125a5abad3af723d0a2ae6119b6dd188678f77
Darren Siegel <siegel.darren@gmail.com>
2017-05-31 22:47:34 -0400

    fix filename reference

--- src/editors/content/common/draft/renderers/Activity.tsx
+++ src/editors/content/common/draft/renderers/Activity.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Activity as ActivityType } from '../../../../../data/content/html/Activity';
+import { Activity as ActivityType } from '../../../../../data/content/html/activity';
 import PreformattedText from './PreformattedText';
 import * as persistence from '../../../../../data/persistence';
 import { LegacyTypes } from '../../../../../data/types';
