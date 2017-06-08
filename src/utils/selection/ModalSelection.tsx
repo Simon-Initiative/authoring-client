ce93d9d8fd0a6cb97de05fd47237a76b18b47e47
Darren Siegel <siegel.darren@gmail.com>
2017-06-07 22:07:47 -0400

    convert to using react-modal

--- src/utils/selection/ModalSelection.tsx
+++ src/utils/selection/ModalSelection.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Modal from 'react-modal';
 
 interface ModalSelection {
   modal: any;
@@ -12,6 +13,53 @@ export interface ModalSelectionProps {
   onCancel: () => void;
 }
 
+const tempnavstyle = {   
+  objectContainer: {
+    marginTop: '10px',
+    overflow: 'auto',
+  },
+};
+
+const customStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    zIndex: 1000 // otherwise we're behind the left most nav bar
+  },
+  content: {
+    position: 'absolute',
+    top: '140px',
+    left: '140px',
+    right: '140px',
+    bottom: '140px',
+    border: '1px solid #444444',
+    background: '#fff',
+    overflow: 'auto',
+    WebkitOverflowScrolling: 'touch',
+    borderRadius: '4px',
+    outline: 'none',
+    padding: '20px',
+    display : "flex", 
+    flexDirection : 'column'  
+  },
+  messagePanel: {
+    height: "150px",
+    lineHeight: "150px",
+    border: "1px solid black",
+    margin: "auto"
+  },
+  messageSpan : {
+    fontSize: "24pt",  
+    display: "inline-block",
+    verticalAlign: "middle",
+    lineHeight: "24pt"
+  }  
+};
+
 class ModalSelection extends React.PureComponent<ModalSelectionProps, {}> {
 
   render() {
@@ -19,34 +67,21 @@ class ModalSelection extends React.PureComponent<ModalSelectionProps, {}> {
     const okLabel = this.props.okLabel !== undefined ? this.props.okLabel : 'Insert';
     const cancelLabel = this.props.cancelLabel !== undefined ? this.props.cancelLabel : 'Cancel';
 
-    return (      
-      <div ref={(modal) => { this.modal = modal; }} className="modal fade">
-        <div className="modal-dialog modal-lg" role="document">
-            <div className="modal-content">
-            <div className="modal-header">
-                <h5 className="modal-title">{this.props.title}</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div className="modal-body">
-                {this.props.children}
-            </div>
-            <div className="modal-footer">
-                <button onClick={this.props.onCancel} 
-                  type="button" data-dismiss="modal" 
-                  className="btn btn-secondary">{cancelLabel}</button>
-                <button onClick={this.props.onInsert} 
-                  type="button" data-dismiss="modal" 
-                  className="btn btn-primary">{okLabel}</button>
-            </div>
-            </div>
-        </div>
-      </div>);
-  }
+    return (  
+      <Modal
+        isOpen={true}
+        contentLabel={this.props.title}
+        style={customStyles}>
+          <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+            <h2>{this.props.title}</h2>
+            <a className="nav-link" href="#" onClick={e => this.props.onCancel()}>{cancelLabel}</a>
+            <a className="nav-link" href="#" onClick={e => this.props.onInsert()}>{okLabel}</a>
+          </nav>
+          <div style={tempnavstyle.objectContainer}>
+            {this.props.children}                 
+          </div>
+      </Modal>);
 
-  componentDidMount() {
-    (window as any).$(this.modal).modal('show');
   }
 
 }
