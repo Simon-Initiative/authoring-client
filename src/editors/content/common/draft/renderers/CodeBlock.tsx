f361803b89c7f03f7797b2222d1e3ce94681efdc
Darren Siegel <siegel.darren@gmail.com>
2017-06-16 08:33:57 -0400

    support all codeblock attrs

--- src/editors/content/common/draft/renderers/CodeBlock.tsx
+++ src/editors/content/common/draft/renderers/CodeBlock.tsx
@@ -4,7 +4,7 @@ import PreformattedText from './PreformattedText';
 import { InteractiveRenderer, InteractiveRendererProps, 
   InteractiveRendererState} from './InteractiveRenderer';
 import { BlockProps } from './properties';
-import { Select } from '../../Select';
+import { Select, Checkbox, TextInput } from '../../controls';
 
 import './markers.scss';
 
@@ -32,6 +32,9 @@ class CodeBlock extends InteractiveRenderer<CodeBlockProps, CodeBlockState> {
 
     this.onSourceEdit = this.onSourceEdit.bind(this);
     this.onSyntaxChange = this.onSyntaxChange.bind(this);
+    this.onNumberEdit = this.onNumberEdit.bind(this);
+    this.onStartEdit = this.onStartEdit.bind(this);
+    this.onHighlightEdit = this.onHighlightEdit.bind(this);
   }
 
   onSourceEdit(source) {
@@ -42,14 +45,27 @@ class CodeBlock extends InteractiveRenderer<CodeBlockProps, CodeBlockState> {
     this.props.blockProps.onEdit({ codeblock: this.props.data.codeblock.with({ syntax }) });
   }
 
+  onNumberEdit(number) {
+    this.props.blockProps.onEdit({ codeblock: this.props.data.codeblock.with({ number }) });
+  }
+
+  onHighlightEdit(highlight) {
+    this.props.blockProps.onEdit({ codeblock: this.props.data.codeblock.with({ highlight }) });
+  }
+
+  onStartEdit(start) {
+    this.props.blockProps.onEdit({ codeblock: this.props.data.codeblock.with({ start }) });
+  }
+
   render() : JSX.Element {
 
     const syntax = this.props.data.codeblock.syntax;
 
     return (
       <div ref={c => this.focusComponent = c} onFocus={this.onFocus} onBlur={this.onBlur}>
-        <Select editMode={this.state.editMode} 
-          label="Syntax" value={syntax} onChange={this.onSyntaxChange}>
+        <form className="form-inline">
+          <Select editMode={this.props.blockProps.editMode}
+            label="Syntax" value={syntax} onChange={this.onSyntaxChange}>
             <option value="actionscript3">ActionScript</option>
             <option value="bash">Bash</option>
             <option value="c">C</option>
@@ -60,6 +76,35 @@ class CodeBlock extends InteractiveRenderer<CodeBlockProps, CodeBlockState> {
             <option value="text">Text</option>
             <option value="xml">XML</option>
           </Select>
+          <Checkbox
+            editMode={this.props.blockProps.editMode}
+            label="Show line numbers"
+            value={this.props.data.codeblock.number}
+            onEdit={this.onNumberEdit}
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Start number
+          &nbsp;
+          <TextInput
+            editMode={this.props.blockProps.editMode}
+            width="50"
+            type="number"
+            label=""
+            value={this.props.data.codeblock.start}
+            onEdit={this.onStartEdit}
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Highlight
+          &nbsp;
+          <TextInput
+            editMode={this.props.blockProps.editMode}
+            width="70"
+            type="text"
+            label=""
+            value={this.props.data.codeblock.highlight}
+            onEdit={this.onHighlightEdit}
+          />
+        </form>
         <PreformattedText onEdit={this.onSourceEdit} 
           src={this.props.data.codeblock.source} editMode={this.props.blockProps.editMode} />
       </div>);
