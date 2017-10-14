237dc88330bfc008dae74cbcea42f8fa7864dfd7
Darren Siegel <siegel.darren@gmail.com>
2017-10-14 13:58:07 -0400

    simplify nav bar impl

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -11,7 +11,7 @@ import * as view from '../actions/view';
  *
  */
 interface NavigationBarState {
-  closed: boolean;
+  
 }
 
 /**
@@ -21,23 +21,6 @@ export interface NavigationBarOwnProps {
   viewActions: any;
 }
 
-/**
- *
- */
-function FoldInButton(props) {
-  return (
-    <a onClick={props.onClick}>Collapse Menu</a>
-  );
-}
-
-/**
- *
- */
-function FoldOutButton(props) {
-  return (
-    <a onClick={props.onClick}>Open</a>
-  );
-}
 
 // Nick, do whatever you feel you have to here
 const navbarStyles =
@@ -89,12 +72,22 @@ const navbarStyles =
     },
   };
 
-interface MenuItem {
-  label?: string;
-  icon?: string;
-  staticContent?: boolean;
-  onclick?: any;
-}
+// tslint:disable-next-line
+const Section = (props) => {
+  return <h2 key={props.label}>{props.label}</h2>;
+};
+
+// tslint:disable-next-line
+const Content = (props) => {
+  return (
+    <li key={props.label} className="nav-item">
+      <a className="nav-link" onClick={props.onClick}>
+        {props.label}
+      </a>
+    </li>
+  );
+};
+
 
 function mapStateToProps(state: any) {
 
@@ -117,180 +110,55 @@ type NavigationBarProps = NavigationBarReduxProps & NavigationBarOwnProps & { di
  *
  */
 class NavigationBar extends React.Component<NavigationBarProps, NavigationBarState> {
-  opts = Immutable.OrderedMap<string, MenuItem>(
-    {
-      package: {
-        label: 'Content Package',
-        icon: 'C',
-        staticContent: false,
-        onclick: this.placeholderMenuHandler,
-      },
-      sequencing: {
-        label: 'Sequencing',
-        staticContent: true,
-        onclick: this.placeholderMenuHandler,
-      },
-      organizations: {
-        label: 'Organizations',
-        icon: 'O',
-        staticContent: false,
-        onclick: this.placeholderMenuHandler,
-      },
-      content: {
-        label: 'Content',
-        staticContent: true,
-        onclick: this.placeholderMenuHandler,
-      },
-      workBookPages: {
-        label: 'Pages',
-        icon: 'O',
-        staticContent: false,
-        onclick: this.placeholderMenuHandler,
-      },
-      activities: {
-        label: 'Assessments',
-        icon: 'A',
-        staticContent: false,
-        onclick: this.placeholderMenuHandler,
-      },
-      pools: {
-        label: 'Question Pools',
-        icon: 'P',
-        staticContent: false,
-        onclick: this.placeholderMenuHandler,
-      },
-      // others: {
-      //   label: 'Others',
-      //   icon: 'M',
-      //   staticContent: false,
-      //   onclick: this.placeholderMenuHandler,
-      // },
-      learning: {
-        label: 'Learning',
-        staticContent: true,
-        onclick: this.placeholderMenuHandler,
-      },
-      objectives: {
-        label: 'Objectives',
-        icon: 'A',
-        staticContent: false,
-        onclick: this.placeholderMenuHandler,
-      },
-      // skills: {
-      //   label: 'Skills',
-      //   icon: 'A',
-      //   staticContent: false,
-      //   onclick: this.placeholderMenuHandler,
-      // },
-      //assets: {
-      //  label: 'Assets',
-      //  staticContent: true,
-      //  onclick: this.placeholderMenuHandler,
-      //},
-      //media: {
-      //  label: 'Files/Media',
-      //  icon: 'M',
-      //  staticContent: false,
-      //  onclick: this.placeholderMenuHandler,
-      //},
-      // ,
-      // addOns: {
-      //   label: "Add-Ons",
-      //   icon: "L",
-      //   staticContent: false,
-      //   onclick: this.placeholderMenuHandler
-      // }
-    });
-
+  
   constructor(props) {
     super(props);
-    this.state = { closed: false };
   }
 
-  handleFoldIn(event: any) {
-    this.setState({ closed: true });
-  }
-
-  handleFoldOut(event: any) {
-    this.setState({ closed: false });
-  }
-
-  /**
-   *
-   */
-  placeholderMenuHandler(props) {
-    console.log('placeHolderMenuHanlder ()');
-  }
-
-  /**
-   *
-   */
-  generateMenuItem(closed: boolean, item: any) {
-    if (item.staticContent === true) {
-      return (<h2 key={item.label}>{item.label}</h2>);
-    }
-
-    if (closed === true) {
-      return (
-        <li key={item.label} className="nav-item"><a className="nav-link" onClick={item.onclick}>{item.icon}</a>
-        </li>);
-    }
-
-    return (
-      <li key={item.label} className="nav-item"><a className="nav-link" onClick={item.onclick}>{item.label}</a>
-      </li>);
-  }
-
-  /**
-   * We included this dedicated menu generator to ensure we could insert main menu options
-   * dynamically from external data and even from a marktplace (yes we can)
-   */
-  generateMenu(closed: boolean) {
-    return (this.opts.toArray().map(item => this.generateMenuItem(closed, item)));
-  }
-
-  /**
-   * Main render function
-   */
   render() {
-    let menuControl = null;
-    let mStyle = null;
-
+    
     const courseId = this.props.course.model.guid;
     
-    this.opts.get('package').onclick = 
-      () => view.viewDocument(courseId, courseId);
-    this.opts.get('organizations').onclick = view.viewOrganizations.bind(undefined, courseId);
-    this.opts.get('workBookPages').onclick = view.viewPages.bind(undefined, courseId);
-    this.opts.get('activities').onclick = view.viewAssessments.bind(undefined, courseId);
-    this.opts.get('objectives').onclick = view.viewObjectives.bind(undefined, courseId);
-    this.opts.get('pools').onclick = view.viewPools.bind(undefined, courseId);
-
-    if (this.state.closed === true) {
-      menuControl = <FoldOutButton onClick={ e => this.handleFoldOut(e) }/>;
-      mStyle = navbarStyles.closedMenu as any;
-    } else {
-      menuControl = <FoldInButton onClick={ e => this.handleFoldIn(e) }/>;
-      mStyle = navbarStyles.openMenu as any;
-    }
-
-    const menuData = this.generateMenu(this.state.closed);
-
-    const title = this.props.course === null || typeof this.props.course === 'undefined' ? '' : this.props.course.model.title;
+    const title = this.props.course === null 
+      || this.props.course === undefined
+      ? '' 
+      : this.props.course.model.title;
 
     return (
-      <nav style={navbarStyles.sidebar as any} className="col-sm-3 col-md-2 hidden-xs-down sidebar">
+      <nav style={navbarStyles.sidebar as any} 
+        className="col-sm-3 col-md-2 hidden-xs-down sidebar">
+        
         <h1>{title}</h1>
         <br/>
         <ul className="nav nav-pills flex-column">
-          {menuData}
-        </ul>
-        <ul className="nav nav-pills flex-column feedback">
-          <li><a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfrkoCCe2cX5KFKcdzmtbLVNPkTSQeiJ4w0mEBqCNrT6hfceA/viewform?usp=sf_link">Feedback</a></li>
+          
+          <Content label="Content Package"
+            onClick={() => view.viewDocument(courseId, courseId)}/>
+
+          <Section label="Sequencing"/>
+          <Content label="Organizations"
+            onClick={view.viewOrganizations.bind(undefined, courseId)}/>
+
+          <Section label="Content"/>
+          <Content label="Pages"
+            onClick={view.viewPages.bind(undefined, courseId)}/>
+          
+          <Section label="Assessments"/>
+          <Content label="Formative"
+            onClick={view.viewAssessments.bind(undefined, courseId)}/>
+          <Content label="Summative"
+            onClick={view.viewAssessments.bind(undefined, courseId)}/>
+          <Content label="Question Pools"
+            onClick={view.viewPools.bind(undefined, courseId)}/>
+          
+          <Section label="Learning"/>
+          <Content label="Objectives"
+            onClick={view.viewObjectives.bind(undefined, courseId)}/>
         </ul>
       </nav>
     );
   }
 }
 
-export default connect<NavigationBarReduxProps, {}, NavigationBarOwnProps>(mapStateToProps)(NavigationBar);
+export default connect<NavigationBarReduxProps, {}, NavigationBarOwnProps>
+  (mapStateToProps)(NavigationBar);
