feffc4a9e08c9f9605288958abded34a4ea8ddcb
Eli Knebel <eliknebel@cmu.edu>
2017-11-06 17:22:44 -0500

    fix tslint errors

--- src/editors/document/common/AbstractEditor.tsx
+++ src/editors/document/common/AbstractEditor.tsx
@@ -7,121 +7,128 @@ import * as types from '../../../data/types';
 import { AppServices } from '../../common/AppServices';
 import { AppContext } from '../../common/AppContext';
 import { handleKey, unhandleKey } from './keyhandlers';
-import { Maybe } from 'tsmonad'; 
+import { Maybe } from 'tsmonad';
 
 
-export interface AbstractEditor<ModelType, P extends AbstractEditorProps<ModelType>, S extends AbstractEditorState> {
-  
+export interface AbstractEditor<ModelType, P extends AbstractEditorProps<ModelType>,
+  S extends AbstractEditorState> {
   undoStack: Immutable.Stack<ModelType>;
   redoStack: Immutable.Stack<ModelType>;
 }
 
 export interface AbstractEditorProps<ModelType> {
-
   // The initial document model passed into the editor.
   model: ModelType;
 
   // Handles edits to the model
-  onEdit: (model: ModelType) => void;  
+  onEdit: (model: ModelType) => void;
 
   // Handles edits to the model driven by undo/redo
-  onUndoRedoEdit: (model: ModelType) => void;  
+  onUndoRedoEdit: (model: ModelType) => void;
 
   services: AppServices;
 
   context: AppContext;
-  
+
   editMode: boolean;
 
   dispatch: any;
 
   expanded: Maybe<Immutable.Set<string>>;
-
-
-
 }
 
 export interface AbstractEditorState {
-  
+
   undoStackSize: number;
 
   redoStackSize: number;
 }
 
 /**
- * An abstract editor that provides the basis for reusable 
- * persistence and undo/redo. 
+ * An abstract editor that provides the basis for reusable
+ * persistence and undo/redo.
  */
-export abstract class AbstractEditor<ModelType, 
+export abstract class AbstractEditor<ModelType,
   P extends AbstractEditorProps<ModelType>, S extends AbstractEditorState>
   extends React.Component<P, S> {
+  constructor(props: P, childState: Object) {
+    super(props);
 
-    constructor(props: P, childState: Object) {
-      super(props);
+    this.undoStack = Immutable.Stack<ModelType>().push(props.model);
+    this.redoStack = Immutable.Stack<ModelType>();
 
-      this.undoStack = Immutable.Stack<ModelType>().push(props.model);
-      this.redoStack = Immutable.Stack<ModelType>();
+    this.state = (
+      Object.assign(
+        {},
+        {
+          undoStackSize: 0,
+          redoStackSize: 0,
+        },
+        childState,
+      ) as S
+    );
+  }
 
-      this.state = (Object.assign({}, { 
-        undoStackSize: 0, 
-        redoStackSize: 0
-      }, childState) as S);
-    }
+  componentDidMount() {
+    handleKey(
+      'ctrl+z',
+      () => this.undoStack.size > 1,
+      this.undo.bind(this));
+    handleKey(
+      'ctrl+y',
+      () => this.redoStack.size > 0,
+      this.redo.bind(this));
+  }
 
-    componentDidMount() {
-      handleKey(
-        'ctrl+z', 
-        () => this.undoStack.size > 1, 
-        this.undo.bind(this));
-      handleKey(
-        'ctrl+y', 
-        () => this.redoStack.size > 0, 
-        this.redo.bind(this));
-    }
+  componentWillUnmount() {
+    unhandleKey('ctrl+z');
+    unhandleKey('ctrl+y');
+  }
 
-    componentWillUnmount() {
-      unhandleKey('ctrl+z');
-      unhandleKey('ctrl+y');
-    }
+  undo() {
+    const currentModel = this.undoStack.peek();
+    this.redoStack = this.redoStack.push(currentModel);
 
-    undo() {
+    this.undoStack = this.undoStack.pop();
 
-      const currentModel = this.undoStack.peek();
-      this.redoStack = this.redoStack.push(currentModel);
+    const model = this.undoStack.peek();
 
-      this.undoStack = this.undoStack.pop();
+    this.setState(
+      {
+        undoStackSize: this.undoStack.size - 1,
+        redoStackSize: this.redoStack.size,
+      },
+      () => this.props.onUndoRedoEdit(model),
+    );
 
-      let model = this.undoStack.peek();
+  }
 
-      this.setState({ 
-        undoStackSize: this.undoStack.size - 1, 
-        redoStackSize: this.redoStack.size
-      }, () => this.props.onUndoRedoEdit(model));
-     
-    }
+  handleEdit(model: ModelType) {
+    this.undoStack = this.undoStack.push(model);
+    this.redoStack = Immutable.Stack<ModelType>();
 
-    handleEdit(model: ModelType) {
-      this.undoStack = this.undoStack.push(model);
-      this.redoStack = Immutable.Stack<ModelType>();
+    this.setState(
+      {
+        undoStackSize: this.undoStack.size - 1,
+        redoStackSize: this.redoStack.size,
+      },
+      () => this.props.onEdit(model),
+    );
+  }
 
-      this.setState({ 
-        undoStackSize: this.undoStack.size - 1, 
-        redoStackSize: this.redoStack.size
-      }, () => this.props.onEdit(model));
+  redo() {
+    const model = this.redoStack.peek();
+    this.undoStack = this.undoStack.push(model);
+    this.redoStack = this.redoStack.pop();
 
-    }
-
-    redo() {
-      
-      const model = this.redoStack.peek();
-      this.undoStack = this.undoStack.push(model);
-      this.redoStack = this.redoStack.pop();
-
-      this.setState({ 
-        undoStackSize: this.undoStack.size - 1, 
-        redoStackSize: this.redoStack.size
-      }, () => this.props.onUndoRedoEdit(model));
-    }
+    this.setState(
+      {
+        undoStackSize: this.undoStack.size - 1,
+        redoStackSize: this.redoStack.size,
+      },
+      () => this.props.onUndoRedoEdit(model),
+    );
+  }
 
 
 }
