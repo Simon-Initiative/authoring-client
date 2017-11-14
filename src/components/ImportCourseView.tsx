7eaa2e99b3512ca185d0d88a57681c6d1c094d21
Darren Siegel <siegel.darren@gmail.com>
2017-11-14 14:32:47 -0500

    add note indicating wait time and behavior

--- src/components/ImportCourseView.tsx
+++ src/components/ImportCourseView.tsx
@@ -14,8 +14,7 @@ export interface ImportCourseViewProps {
 }
 
 export interface ImportCourseViewState {
-  waiting: boolean;
-  error: boolean;
+
 }
 
 class ImportCourseView extends React.PureComponent<ImportCourseViewProps, ImportCourseViewState> {
@@ -23,26 +22,19 @@ class ImportCourseView extends React.PureComponent<ImportCourseViewProps, Import
   constructor(props) {
     super(props);
     this.onCancel = this.onCancel.bind(this);
-
-    this.state = {
-      waiting: false,
-      error: false,
-    };
+    this.onImport = this.onImport.bind(this);
   }
 
-  startCreation(url: string) {
-    persistence.importPackage(url);
-  }
-
-  import(e) {
-    e.preventDefault();
+  onImport() {
 
     const url = (this.refs['url'] as any).value;
     if (url === undefined || url === null) {
       return;
     }
 
-    this.setState({ waiting: true }, () => this.startCreation(url));
+    persistence.importPackage(url);
+
+    viewActions.viewAllCourses();
   }
 
   onCancel() {
@@ -53,7 +45,7 @@ class ImportCourseView extends React.PureComponent<ImportCourseViewProps, Import
 
     const inputs = (
       <div className="col-md-4 offset-sm-4">
-        <button onClick={this.import.bind(this)}
+        <button onClick={this.onImport.bind(this)}
                 style={{ color: 'white ' }}
                 className="btn btn-secondary btn-lg btn-block outline serif">
           Import Course
@@ -66,42 +58,11 @@ class ImportCourseView extends React.PureComponent<ImportCourseViewProps, Import
       </div>
     );
 
-    const waiting = (
-      <div className="col-md-4 offset-sm-4">
-        <div className="alert alert-info" role="alert">
-          <h4 className="alert-heading">Course Import in Progress</h4>
-          <p>Your course is being imported. </p>
-          <p className="mb-0">This may take several minutes, depending on
-          the size of the course. Upon completion of the import you will
-            be taken to the front page of the new course.
-          </p>
-          <p className="mb-0">If you navigate away from this page before
-          the import completes you
-          can check on the status of the import from the
-          &quot;Course Packages&quot; view.
-          </p>
-        </div>
-      </div>
-    );
-
-    const error = (
-      <div className="col-md-4 offset-sm-4">
-        <div className="alert alert-danger" role="alert">
-          <h4 className="alert-heading">Error!</h4>
-          <p>A problem was encountered trying to import the course</p>
-          <p className="mb-0">Please try again, if the problem persists please
-            contact support.
-          </p>
-        </div>
-      </div>
-    );
-
     return (
       <div className="createCourse full container-fluid">
         <div className="row">
           <div className="col-md-12">
             <h1>Import an existing course content package</h1>
-
           </div>
         </div>
         <div className="row">
@@ -111,12 +72,24 @@ class ImportCourseView extends React.PureComponent<ImportCourseViewProps, Import
             <label htmlFor="input">SVN URL</label>
           </fieldset>
         </div>
+        {inputs}
+        <br/>
         <div className="row">
-          {this.state.waiting
-            ? waiting
-            : this.state.error
-              ? error
-              : inputs }
+          <div className="col-md-8 offset-sm-2">
+            <div className="alert alert-info" role="alert">
+              <h4 className="alert-heading">Note</h4>
+              <p className="mb-0">Importing an existing OLI course package
+              can take several minutes, especially if the course package
+              is large and contains many assets.
+              </p>
+              <br/>
+              <p className="mb-0">After you initiate an import, you will
+              be taken to the
+              &quot;My Course Packages&quot; page, where you can see status
+              of the course package that you are importing.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
