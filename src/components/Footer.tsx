faf318b4d3b0eb1111838b44aed40b1ff7df7e35
Eli Knebel <eliknebel@cmu.edu>
2018-02-09 15:27:11 -0500

    add links to footer and update styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
+import { buildFeedbackFromCurrent } from 'utils/feedback';
 
 import './Footer.scss';
 
-interface Footer {
-}
-
 export interface FooterProps {
-
+  name: string;
+  email: string;
 }
 
 class Footer extends React.PureComponent<FooterProps, {}> {
@@ -16,20 +15,37 @@ class Footer extends React.PureComponent<FooterProps, {}> {
   }
 
   render() {
+    const { name, email } = this.props;
+
+    const formUrl = buildFeedbackFromCurrent(name, email);
+    const CREATIVE_COMMONS_URL = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+    const QUICK_START_GUIDE_URL
+      = 'https://docs.google.com/document/d/1B_AQpFRn2zue6-'
+      + 'nW6h8z6bOGfHWYqAjI7WCZ-WQMrf4/edit?usp=sharing';
+
     return (
       <footer className="footer">
-        <div className="container">
-            <div className="row">
-                <div className="license col-md-10">
-                  <img
-                    className="ccLicense"
-                    src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
-                  Unless otherwise noted this work is licensed under a<span> </span>
-                  <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" rel="license">
-                    Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License
-                  </a>
-                </div>
+        <div className="footer-section left">
+          <div className="quick-links">
+            <a href={QUICK_START_GUIDE_URL} target="_blank">Quick Start Guide</a>
+            {'|'}
+            <a href={formUrl} target="_blank">Help / Feedback</a>
+          </div>
+        </div>
+        <div className="footer-section center">
+          <div className="centered">
+            <a href={CREATIVE_COMMONS_URL} target="_blank" className="ccLicense">
+              <img src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
+            </a>
+            <div className="license">
+              Unless otherwise noted this work is licensed under a<span> </span>
+              <a href={CREATIVE_COMMONS_URL} target="_blank" rel="license">
+                Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License
+              </a>
             </div>
+          </div>
+        </div>
+        <div className="footer-section right">
         </div>
       </footer>
     );
