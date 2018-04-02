765f34c34fe4e089aa4887e33ffd5eae7be158a6
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2018-04-02 10:44:14 -0400

    Fix sidebar issues in linkeditor

--- src/editors/content/learning/LinkEditor.tsx
+++ src/editors/content/learning/LinkEditor.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import { injectSheet, classNames, JSSProps } from 'styles/jss';
+import { injectSheet, JSSProps } from 'styles/jss';
 import * as contentTypes from 'data/contentTypes';
 
 import { AbstractContentEditor, AbstractContentEditorProps } from '../common/AbstractContentEditor';
 import { LinkTarget } from 'data/content/learning/common';
 import { Select, TextInput } from '../common/controls';
-import { Label, VerticalSpacer, Header } from '../common/Sidebar';
+import { SidebarContent } from 'components/sidebar/ContextAwareSidebar.controller';
+import { SidebarGroup } from 'components/sidebar/ContextAwareSidebar';
+import { ToolbarGroup, ToolbarLayout } from 'components/toolbar/ContextAwareToolbar';
+import { ToolbarButton, ToolbarButtonSize } from 'components/toolbar/ToolbarButton';
+import { CONTENT_COLORS } from 'editors/content/utils/content';
 
 import styles from './Entity.style';
 
-
 export interface LinkEditorProps extends AbstractContentEditorProps<contentTypes.Link> {
-  className?: string;
+  onShowSidebar: () => void;
 }
 
 export interface LinkEditorState {
@@ -34,46 +37,50 @@ export default class LinkEditor
   }
 
   renderSidebar() {
-    const { className, classes, editMode, model, onEdit } = this.props;
+    const { editMode, model, onEdit } = this.props;
 
     return (
-      <div className={classNames([classes.entityRenderer, className])}>
+      <SidebarContent title="External Link">
 
-        <Header>External Link</Header>
+        <SidebarGroup label="URL">
+          <TextInput
+            editMode={editMode}
+            width="100%"
+            label=""
+            value={model.href}
+            type="string"
+            onEdit={href => onEdit(model.with({ href }))}
+            />
+        </SidebarGroup>
 
-        <Label>URL</Label>
-        <TextInput
-          editMode={editMode}
-          width="100%"
-          label=""
-          value={model.href}
-          type="string"
-          onEdit={href => onEdit(model.with({ href }))}
-          />
+        <SidebarGroup label="Target">
+          <Select
+            editMode={editMode}
+            value={model.target}
+            onChange={v =>
+              onEdit(model.with({ target: v === 'self' ? LinkTarget.Self : LinkTarget.New }))}>
+            <option value={LinkTarget.Self}>Open in this window</option>
+            <option value={LinkTarget.New}>Open in new window</option>
+          </Select>
+        </SidebarGroup>
 
-        <VerticalSpacer/>
-
-        <Label>Target</Label>
-        <Select
-          editMode={editMode}
-          value={model.target}
-          onChange={v =>
-            onEdit(model.with({ target: v === 'self' ? LinkTarget.Self : LinkTarget.New }))}>
-          <option value={LinkTarget.Self}>Open in this window</option>
-          <option value={LinkTarget.New}>Open in new window</option>
-        </Select>
-      </div>
+      </SidebarContent>
     );
   }
 
   renderToolbar() {
-    const { className, classes } = this.props;
-
     return (
-      <div className={classNames([classes.entityRenderer, className])}>
-        Link
-      </div>
+      <ToolbarGroup
+        label="External Link"
+        highlightColor={CONTENT_COLORS.Xref}
+        columns={2}>
+        <ToolbarLayout.Column>
+          <ToolbarButton onClick={this.props.onShowSidebar} size={ToolbarButtonSize.Large}>
+            <div><i className="fa fa-sliders"/></div>
+            <div>Details</div>
+          </ToolbarButton>
+        </ToolbarLayout.Column>
+      </ToolbarGroup>
     );
   }
-
 }
