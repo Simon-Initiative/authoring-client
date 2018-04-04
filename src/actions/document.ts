795fd8fa5c8a44db888a717778e27e44fac80a2c
Eli Knebel <eliknebel@cmu.edu>
2018-04-04 13:52:59 -0400

    move doc release action to before request response

--- src/actions/document.ts
+++ src/actions/document.ts
@@ -213,10 +213,10 @@ export function load(courseId: string, documentId: string) {
 
 export function release(documentId: string) {
   return function (dispatch, getState) {
+    dispatch(documentReleased(documentId));
 
     const editedDocument: EditedDocument = getState().documents.get(documentId);
-    editedDocument.persistence.destroy()
-      .then(result => dispatch(documentReleased(documentId)));
+    editedDocument.persistence.destroy();
   };
 }
 
