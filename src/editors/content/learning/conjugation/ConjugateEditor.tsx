2565a67aad878cad0c0823e928f2df8847509b40
Darren Siegel <siegel.darren@gmail.com>
2018-05-14 13:20:52 -0400

    Authoring 1073 conjugation (#208)
    * conjugation support
    
    * code cleanup, add icons
    
    * restore try-catch
    
    * increase toolbar sizes
    
    * add element type to conjugation
    
    * add onpaste stub to conjugationEditor.tsx
    
    * fix build errors

---
+++ src/editors/content/learning/conjugation/ConjugateEditor.tsx
@@ -0,0 +1,320 @@
+import * as React from 'react';
+import * as Immutable from 'immutable';
+import * as contentTypes from 'data/contentTypes';
+import { injectSheet, classNames } from 'styles/jss';
+import { StyledComponentProps } from 'types/component';
+import {
+  AbstractContentEditor, AbstractContentEditorProps, RenderContext,
+} from 'editors/content/common/AbstractContentEditor';
+import { ContentContainer } from 'editors/content/container/ContentContainer';
+import { SidebarContent } from 'components/sidebar/ContextAwareSidebar.controller';
+import { SidebarGroup } from 'components/sidebar/ContextAwareSidebar';
+import { ToolbarGroup } from 'components/toolbar/ContextAwareToolbar';
+import { ToolbarButton, ToolbarButtonSize } from 'components/toolbar/ToolbarButton';
+import { CONTENT_COLORS } from 'editors/content/utils/content';
+import { CONJUGATE_ELEMENTS } from 'data/content/learning/conjugate';
+import { TextInput, Button } from '../../common/controls';
+import { getEditorByContentType } from 'editors/content/container/registry';
+import { Maybe } from 'tsmonad';
+import { modalActions } from 'actions/modal';
+import { buildUrl } from 'utils/path';
+import { TextSelection } from 'types/active';
+import { selectAudio } from '../AudioEditor';
+
+import { styles } from './Conjugation.styles';
+
+export interface ConjugateEditorProps
+  extends AbstractContentEditorProps<contentTypes.ConjugationCell> {
+  onShowSidebar: () => void;
+}
+
+export interface ConjugateEditorState {
+  activeChildGuid: string;
+}
+
+/**
+ * The content editor for table cells.
+ */
+@injectSheet(styles)
+export default class ConjugateEditor
+    extends AbstractContentEditor<contentTypes.ConjugationCell,
+    StyledComponentProps<ConjugateEditorProps>, ConjugateEditorState> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeChildGuid: null,
+    };
+
+    this.onFocus = this.onFocus.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps: ConjugateEditorProps) {
+    const { activeChildGuid } = this.state;
+
+    if (nextProps.activeContentGuid !== activeChildGuid) {
+      this.setState({
+        activeChildGuid: null,
+      });
+    }
+  }
+
+  onContentEdit(content, src) {
+
+    if (this.props.model.contentType === 'CellHeader') {
+      const model = this.props.model.with({ content });
+      this.props.onEdit(model, src);
+    } else {
+      const model = this.props.model.with({ content });
+      this.props.onEdit(model, src);
+    }
+
+  }
+
+  onFocus(model: Object, parent, textSelection) {
+    const { onFocus } = this.props;
+
+    this.setState(
+      { activeChildGuid: (model as any).guid },
+      () => onFocus(model, parent, textSelection),
+    );
+  }
+
+  render() : JSX.Element {
+
+    const renderContext = this.props.renderContext === undefined
+      ? RenderContext.MainEditor
+      : this.props.renderContext;
+
+    if (renderContext === RenderContext.Toolbar) {
+      return this.renderToolbar();
+    }
+    if (renderContext === RenderContext.Sidebar) {
+      return this.renderSidebar();
+    }
+    return (
+      <div style={ { height: '100%' } }
+        onFocus={e => this.handleOnFocus(e)} onClick={e => this.handleOnClick(e)}>
+        {this.renderMain()}
+      </div>
+    );
+
+  }
+
+  onPronounsChange(model: contentTypes.Conjugate, pronouns) {
+    this.props.onEdit(model.with({ pronouns }));
+  }
+
+  onSelect() {
+    const { context, services, onEdit, model } = this.props;
+
+    const dispatch = (services as any).dispatch;
+    const dismiss = () => dispatch(modalActions.dismiss());
+    const display = c => dispatch(modalActions.display(c));
+
+    if (model.contentType === 'Conjugate') {
+      const src1 = new contentTypes.Source().with({ src: model.src });
+      const inputAudio = model.src !== ''
+        ? new contentTypes.Audio().with({
+          sources: Immutable.OrderedMap<string, contentTypes.Source>()
+            .set(src1.guid, src1),
+        })
+        : null;
+
+      selectAudio(inputAudio, context.resourcePath, context.courseModel, display, dismiss)
+        .then((audio) => {
+          if (audio !== null) {
+            const src = audio.sources.first().src;
+            const type = 'audio/mp3';
+            if (model.contentType === 'Conjugate') {
+              const updated = model.with({ src, type });
+              onEdit(updated, updated);
+            }
+          }
+        });
+    }
+
+  }
+
+  onAudioRemove(model: contentTypes.Conjugate) {
+    const updated = model.with({ src: '' });
+    this.props.onEdit(updated, updated);
+  }
+
+  renderAudio(model: contentTypes.Conjugate) {
+
+    let fullSrc = '';
+
+    const src = model.src;
+    fullSrc = buildUrl(
+      this.props.context.baseUrl,
+      this.props.context.courseId,
+      this.props.context.resourcePath,
+      src);
+
+    return (
+      <div>
+        <audio src={fullSrc} controls={true}/>
+        <Button
+          editMode={this.props.editMode}
+          onClick={this.onAudioRemove.bind(this, model)}>Remove Audio</Button>
+      </div>
+    );
+
+  }
+
+
+  renderSidebarConjugate(model: contentTypes.Conjugate) {
+    const { editMode } = this.props;
+    const { pronouns } = model;
+
+    const audio = model.src !== ''
+      ? this.renderAudio(model)
+      : <div>Select an audio file from the toolbar</div>;
+
+    return (
+      <SidebarContent title="Conjugate">
+        <SidebarGroup label="Pronouns">
+          <TextInput
+            editMode={editMode}
+            value={pronouns}
+            type="text"
+            width="100%"
+            label=""
+            onEdit={this.onPronounsChange.bind(this, model)}
+          />
+        </SidebarGroup>
+        <SidebarGroup label="Audio">
+          {audio}
+        </SidebarGroup>
+      </SidebarContent>
+    );
+  }
+
+  renderSidebarHeader() {
+
+    return (
+      <SidebarContent title="Header">
+      </SidebarContent>
+    );
+  }
+
+  renderSidebar() {
+    if (this.props.model.contentType === 'CellHeader') {
+      return this.renderSidebarHeader();
+    }
+    return this.renderSidebarConjugate(this.props.model);
+  }
+
+  renderToolbarHeader() {
+    const { onShowSidebar } = this.props;
+
+    return (
+      <ToolbarGroup label="Conjugate Header"
+        columns={4} highlightColor={CONTENT_COLORS.Conjugate}>
+        <ToolbarButton onClick={() => onShowSidebar()} size={ToolbarButtonSize.Large}>
+          <div><i className="fa fa-th-list"></i></div>
+          <div>Details</div>
+        </ToolbarButton>
+      </ToolbarGroup>
+    );
+  }
+
+  renderToolbarConjugate(model: contentTypes.Conjugate) {
+    const { onShowSidebar } = this.props;
+
+    return (
+      <ToolbarGroup label="Conjugate"
+        columns={4} highlightColor={CONTENT_COLORS.Conjugate}>
+        <ToolbarButton onClick={this.onSelect} size={ToolbarButtonSize.Large}>
+            <div><i className="fa fa-music"/></div>
+            <div>Select Audio</div>
+          </ToolbarButton>
+        <ToolbarButton onClick={() => onShowSidebar()} size={ToolbarButtonSize.Large}>
+          <div><i className="fa fa-th-list"></i></div>
+          <div>Details</div>
+        </ToolbarButton>
+      </ToolbarGroup>
+    );
+  }
+
+  renderToolbar() {
+    if (this.props.model.contentType === 'CellHeader') {
+      return this.renderToolbarHeader();
+    }
+    return this.renderToolbarConjugate(this.props.model);
+  }
+
+  renderSimplifiedTextEditor() {
+    const parent = {
+      supportedElements: Immutable.List<string>(CONJUGATE_ELEMENTS),
+      onEdit(content: Object, source: Object) {
+        this.props.onEdit(this.props.model.with({ content }), source);
+      },
+      onAddNew(content: Object, textSelection: Maybe<TextSelection>) {},
+      onRemove(content: Object) {},
+      onDuplicate(content: Object) {},
+      onMoveUp(content: Object) {},
+      onMoveDown(content: Object) {},
+      props: this.props,
+    };
+
+    const props = Object.assign({}, this.props, {
+      hideBorder: true,
+      model: this.props.model.content,
+      onEdit: this.onContentEdit.bind(this),
+      parent: this,
+      onClick: () => {},
+      onFocus: (m, p, t) => this.onFocus(m, parent, t),
+    });
+
+    return React.createElement(
+      getEditorByContentType('ContiguousText'), props);
+  }
+
+  renderMain() : JSX.Element {
+    const { className, classes, model, parent, activeContentGuid } = this.props;
+    const { activeChildGuid } = this.state;
+
+    const cellClass =
+      activeContentGuid === model.guid
+      ? classes.innerConjugateSelected : classes.innerConjugate;
+
+    const bindProps = (element) => {
+
+      if (element instanceof contentTypes.ContiguousText) {
+        return [{ propertyName: 'hideBorder', value: true }];
+      }
+      return [];
+    };
+
+    const hideDecorator = model.content.content.size === 0 ||
+     (model.content.content.size === 1
+      && model.content.content.first().contentType === 'ContiguousText');
+
+    const contentEditor = this.props.model.contentType === 'Conjugate'
+      ? this.renderSimplifiedTextEditor()
+      : <ContentContainer
+          {...this.props}
+          onFocus={this.onFocus}
+          hideSingleDecorator={hideDecorator}
+          bindProperties={bindProps}
+          model={this.props.model.content}
+          onEdit={this.onContentEdit.bind(this)}
+        />;
+
+    return (
+      <div className={classNames([
+        cellClass, className, activeChildGuid && classes.innerConjugateChildSelected])}
+        onClick={() => this.props.onFocus(model, parent, Maybe.nothing())}>
+        <div>
+          {contentEditor}
+        </div>
+        <i className={classNames(['fa fa-caret-square-o-down', classes.selectConjugate])} />
+      </div>
+    );
+  }
+
+}
---
+++ src/editors/content/learning/conjugation/ConjugationEditor.tsx
@@ -0,0 +1,515 @@
+import * as React from 'react';
+import * as Immutable from 'immutable';
+import * as contentTypes from 'data/contentTypes';
+import { injectSheet, classNames } from 'styles/jss';
+import { StyledComponentProps } from 'types/component';
+import {
+  AbstractContentEditor, AbstractContentEditorProps,
+} from 'editors/content/common/AbstractContentEditor';
+import { SidebarContent } from 'components/sidebar/ContextAwareSidebar.controller';
+import { ContentContainer } from 'editors/content/container/ContentContainer';
+import { ContentElement } from 'data/content/common/interfaces';
+import { ContentElements } from 'data/content/common/elements';
+import { ToolbarGroup } from 'components/toolbar/ContextAwareToolbar';
+import { ToolbarDropdown, ToolbarDropdownSize } from 'components/toolbar/ToolbarDropdown';
+import { CONTENT_COLORS } from 'editors/content/utils/content';
+import { TextInput } from '../../common/controls';
+import { TitleTextEditor } from 'editors/content/learning/contiguoustext/TitleTextEditor';
+import { ContiguousText } from 'data/content/learning/contiguous';
+
+import ConjugateEditor from './ConjugateEditor';
+import { Maybe } from 'tsmonad';
+import { isFirefox, isEdge, isIE } from 'utils/browser';
+import {
+  DiscoverableId,
+} from 'components/common/Discoverable.controller';
+
+import { styles } from './Conjugation.styles';
+
+export interface ConjugationEditorProps
+  extends AbstractContentEditorProps<contentTypes.Conjugation> {
+  onShowSidebar: () => void;
+  onDiscover: (id: DiscoverableId) => void;
+}
+
+export interface ConjugationEditorState {
+
+}
+
+// Get the key of the nth element in an ordered map
+function getKey(
+  index: number, collection:
+  Immutable.OrderedMap<string,
+    contentTypes.ConjugationCell | contentTypes.Cr>) {
+  return collection.toArray()[index].guid;
+}
+
+/**
+ * The content editor for tables.
+ */
+@injectSheet(styles)
+export default class ConjugationEditor
+    extends AbstractContentEditor<contentTypes.Conjugation,
+    StyledComponentProps<ConjugationEditorProps>, ConjugationEditorState> {
+  selectionState: any;
+
+  constructor(props) {
+    super(props);
+
+    this.onInsertColumn = this.onInsertColumn.bind(this);
+    this.onInsertRow = this.onInsertRow.bind(this);
+    this.onRemoveRow = this.onRemoveRow.bind(this);
+    this.onRemoveColumn = this.onRemoveColumn.bind(this);
+    this.onTitleEdit = this.onTitleEdit.bind(this);
+  }
+
+  renderSidebar() {
+
+    return (
+      <SidebarContent title="Conjugation">
+      </SidebarContent>
+    );
+  }
+
+  renderToolbar() {
+
+    return (
+      <ToolbarGroup label="Conjugation" columns={4} highlightColor={CONTENT_COLORS.Conjugation}>
+      </ToolbarGroup>
+    );
+  }
+
+  onCellEdit(row, cell, src) {
+
+    const updatedRow = row.with({ cells: row.cells.set(cell.guid, cell) });
+    const model = this.props.model
+      .with({ rows: this.props.model.rows.set(updatedRow.guid, updatedRow) });
+
+    this.props.onEdit(model, src);
+  }
+
+
+  renderCell(row: contentTypes.Cr, cell: contentTypes.ConjugationCell) {
+
+    const { className, classes } = this.props;
+
+    // Passing this fake parent to the CellEditor so that the
+    // empty supportedElements causes all Insert Toolbar buttons
+    // to be disabled, but allows editing of the cell's attributes
+
+    // For now, we disable duplication, removal, and reordering of cells.
+    // This doesn't disable the buttons, though.
+    const noManualControl = {
+      supportedElements: Immutable.List<string>(),
+      onAddNew: (e) => {},
+      onEdit: (e, s) => {
+        this.onCellEdit.call(this, row, e, s);
+      },
+      onPaste: (e) => {},
+      onRemove: (e) => {},
+      onDuplicate: (e) => {},
+      onMoveUp: (e) => {},
+      onMoveDown: (e) => {},
+      props: this.props,
+    };
+
+    const style = {};
+    if (!isFirefox && !isIE && !isEdge) {
+      style['height'] = '1px';
+    }
+
+    const cellEditor = <ConjugateEditor
+      {...this.props}
+      model={cell}
+      parent={noManualControl}
+      onEdit={this.onCellEdit.bind(this, row)}
+    />;
+
+    if (cell.contentType === 'Conjugate') {
+      return (
+        <td
+          key={cell.guid}
+          style={style}
+          className={classNames([classes.cell, className])}>
+          {cellEditor}
+        </td>
+      );
+    }
+    return (
+      <th
+        key={cell.guid}
+        style={style}
+        className={classNames([classes.cell, className])}>
+        {cellEditor}
+      </th>
+    );
+
+
+  }
+
+  renderHeaderRow(columns: number) {
+
+    const { classes, className } = this.props;
+
+    const headers = [];
+    for (let i = 0; i < columns; i += 1) {
+      headers.push(
+        this.renderColumnHeader(i),
+      );
+    }
+
+    return (
+      <tr key="headerRow">
+        <td className={classNames([classes.cornerHeader, className])}></td>
+        {headers}
+      </tr>
+    );
+  }
+
+  renderRowHeader(index: number) {
+
+    const { classes, className } = this.props;
+
+    return (
+      <td
+        key={'row-' + index}
+        className={classNames([classes.rowHeader, className])}>
+        {this.renderRowDropdown(
+          index, this.onInsertRow, this.onInsertHeaderRow, this.onRemoveRow, false)}
+      </td>
+    );
+
+  }
+
+  renderColumnHeader(index: number) {
+
+    const { classes, className } = this.props;
+
+    return (
+      <td
+        key={'column-' + index}
+        className={classNames([classes.colHeader, className])}>
+        {this.renderColumnDropdown(
+          index, this.onInsertColumn,
+          this.onInsertHeaderColumn, this.onRemoveColumn, true)}
+      </td>
+    );
+
+  }
+
+  insertAt(model, toInsert, index) {
+    const arr = model
+      .map((v, k) => [k, v])
+      .toArray();
+
+    arr.splice(index, 0, [toInsert.guid, toInsert]);
+
+    return Immutable.OrderedMap<string, any>(arr);
+  }
+
+  onInsertRow(index: number) {
+
+    const columnsToAdd = this.props.model.rows.last().cells.size;
+
+    const kvPairs = [];
+    for (let i = 0; i < columnsToAdd; i += 1) {
+      const cell = new contentTypes.CellHeader();
+      kvPairs.push([cell.guid, cell]);
+    }
+
+    const cells = Immutable.OrderedMap
+      <string, contentTypes.ConjugationCell>(kvPairs);
+
+    const row = new contentTypes.Cr({ cells });
+    const rows = this.insertAt(this.props.model.rows, row, index);
+
+    this.props.onEdit(this.props.model.with({ rows }), row);
+  }
+
+
+  onInsertHeaderRow(index: number) {
+
+    const columnsToAdd = this.props.model.rows.last().cells.size;
+
+    const kvPairs = [];
+    for (let i = 0; i < columnsToAdd; i += 1) {
+      const cell = new contentTypes.CellHeader();
+      kvPairs.push([cell.guid, cell]);
+    }
+
+    const cells = Immutable.OrderedMap
+      <string, contentTypes.ConjugationCell>(kvPairs);
+
+    const row = new contentTypes.Cr({ cells });
+    const rows = this.insertAt(this.props.model.rows, row, index);
+
+    this.props.onEdit(this.props.model.with({ rows }), row);
+  }
+
+
+
+  onInsertColumn(index: number) {
+
+    const { model } = this.props;
+    let i = 0;
+
+    const rows = model.rows.map((row) => {
+
+      let cell : contentTypes.ConjugationCell = new contentTypes.Conjugate();
+
+      // Use as a template the last cell in the first row, that
+      // way if it is a row of headers in place we continue it
+      if (i === 0 && row.cells.last().contentType === 'CellHeader') {
+        cell = new contentTypes.CellHeader();
+      }
+      i = i + 1;
+
+      const cells = this.insertAt(row.cells, cell, index);
+      return row.with({ cells });
+
+    }).toOrderedMap();
+
+    this.props.onEdit(model.with({ rows }));
+  }
+
+
+  onPronunciationEdit(pronunciations, src) {
+
+    if (pronunciations.content.size === 0) {
+      const model = this.props.model.with({
+        pronunciation: Maybe.nothing(),
+      });
+
+      this.props.onEdit(model, src);
+    } else {
+      const model = this.props.model.with({
+        pronunciation: Maybe.just(pronunciations.content.first()),
+      });
+
+      this.props.onEdit(model, src);
+    }
+  }
+
+  onAddPronunciation() {
+
+    const pronunciation = new contentTypes.Pronunciation();
+    const model = this.props.model.with({
+      pronunciation: Maybe.just(pronunciation),
+    });
+
+    this.props.onEdit(model, pronunciation);
+  }
+
+  onInsertHeaderColumn(index: number) {
+
+    const { model } = this.props;
+
+    const rows = model.rows.map((row) => {
+
+      const cell = new contentTypes.CellHeader();
+      const cells = this.insertAt(row.cells, cell, index);
+      return row.with({ cells });
+
+    }).toOrderedMap();
+
+    this.props.onEdit(model.with({ rows }));
+  }
+
+  onRemoveRow(index: number) {
+
+    if (this.props.model.rows.size > 1) {
+      const rows = this.props.model.rows.delete(getKey(index, this.props.model.rows));
+      this.props.onEdit(this.props.model.with({ rows }));
+    }
+
+  }
+
+  onEditVerb(verb: string) {
+    const model = this.props.model.with({ verb });
+    this.props.onEdit(model, model);
+  }
+
+  onRemoveColumn(index: number) {
+    const model = this.props.model;
+    let rows = model.rows;
+
+    // Do not allow removal of only column
+    if (model.rows.first() && model.rows.first().cells.size <= 1) {
+      return;
+    }
+
+    rows = rows.map((row) => {
+
+      const before = row.cells.toSeq().slice(0, index);
+      const after = row.cells.toSeq().slice(index + 1);
+      const cells = before.concat(after).toOrderedMap();
+
+      return row.with({ cells });
+
+    }).toOrderedMap();
+
+    this.props.onEdit(model.with({ rows }));
+  }
+
+  renderRowDropdown(
+    index: number, onInsert: (index: number) => void,
+    onInsertHeader: (index: number) => void,
+    onRemove: (index: number) => void, showOnRight: boolean) {
+
+    const { classes, className, editMode } = this.props;
+    return (
+      <div className={classNames([classes.dropdown, className])}>
+        <ToolbarDropdown
+          size={ToolbarDropdownSize.Tiny}
+          hideArrow
+          positionMenuOnRight={showOnRight}
+          label={<i className={classNames(['fa fa-ellipsis-v', classes.dropdownLabel,
+            classes.moreLabel])}/>} >
+          <button className="dropdown-item"
+            disabled={!editMode}
+            onClick={() => onInsert(index) }>
+            {`Insert row before`}
+          </button>
+          <button className="dropdown-item"
+            disabled={!editMode}
+            onClick={() => onInsert(index + 1) }>
+            {`Insert row after`}
+          </button>
+          <button className="dropdown-item"
+            disabled={!editMode}
+            onClick={() => onInsertHeader(index) }>
+            {`Insert header row before`}
+          </button>
+          <button className="dropdown-item"
+            disabled={!editMode}
+            onClick={() => onInsertHeader(index + 1) }>
+            {`Insert header row after`}
+          </button>
+          <button className="dropdown-item"
+            disabled={!editMode}
+            onClick={() => onRemove(index) }>
+            {`Remove row`}
+          </button>
+        </ToolbarDropdown>
+      </div>
+    );
+  }
+
+  renderColumnDropdown(
+    index: number, onInsert: (index: number) => void,
+    onInsertHeader: (index: number) => void,
+    onRemove: (index: number) => void,
+    showOnRight: boolean) {
+
+    const { classes, className, editMode } = this.props;
+    return (
+      <div className={classNames([classes.dropdown, className])}>
+        <ToolbarDropdown
+          size={ToolbarDropdownSize.Tiny}
+          hideArrow
+          positionMenuOnRight={showOnRight}
+          label={<i className={classNames(['fa fa-ellipsis-v', classes.dropdownLabel,
+            classes.moreLabel])}/>} >
+          <button className="dropdown-item"
+            disabled={!editMode}
+            onClick={() => onRemove(index) }>
+            {`Remove column`}
+          </button>
+        </ToolbarDropdown>
+      </div>
+    );
+  }
+
+  onTitleEdit(ct: ContiguousText, sourceObject) {
+    const content = this.props.model.title.text.content.set(ct.guid, ct);
+    const text = this.props.model.title.text.with({ content });
+    const title = this.props.model.title.with({ text });
+    const model = this.props.model.with({ title });
+
+    this.props.onEdit(model, sourceObject);
+  }
+
+  renderMain() : JSX.Element {
+
+    const { className, classes, model, editMode } = this.props;
+
+    let maxColumns = 0;
+    const rows = model.rows.toArray().map((row, i) => {
+
+      maxColumns = Math.max(maxColumns, row.cells.size);
+
+      return (
+        <tr key={row.guid}>
+          {this.renderRowHeader(i)}
+          {row.cells.toArray().map(cell => this.renderCell(row, cell))}
+        </tr>
+      );
+    });
+
+    const headerRow = this.renderHeaderRow(maxColumns);
+
+    const canAddPronunciation = model.pronunciation.caseOf({
+      just: n => false,
+      nothing: () => true,
+    });
+
+    const pronunciations = new ContentElements().with({
+      content: model.pronunciation.caseOf({
+        just: p => Immutable.OrderedMap<string, ContentElement>().set(p.guid, p),
+        nothing: () => Immutable.OrderedMap<string, ContentElement>(),
+      }),
+    });
+
+    const pronunciationEditor = model.pronunciation.caseOf({
+      just: p => <ContentContainer
+      {...this.props}
+      model={pronunciations}
+      onEdit={this.onPronunciationEdit.bind(this)}
+    />,
+      nothing: () => null,
+    });
+
+    return (
+      <div className={classNames(['ConjugationEditor', classes.conjugationEditor, className])}>
+
+        <TitleTextEditor
+          context={this.props.context}
+          services={this.props.services}
+          onFocus={this.props.onFocus}
+          model={(this.props.model.title.text.content.first() as ContiguousText)}
+          editMode={this.props.editMode}
+          onEdit={this.onTitleEdit}
+          editorStyles={{ fontSize: 20 }} />
+
+        <TextInput
+          editMode={editMode}
+          width="100px"
+          label="Verb"
+          value={model.verb}
+          type="string"
+          onEdit={this.onEditVerb.bind(this)}
+        />
+        {pronunciationEditor}
+        <button type="button"
+          disabled={!editMode || !canAddPronunciation}
+          onClick={this.onAddPronunciation.bind(this)}
+          className="btn btn-link">+ Add pronunciation</button>
+        <table className={classNames([classes.conjugation, className])}>
+          <tbody>
+          {headerRow}
+          {rows}
+          </tbody>
+        </table>
+        <button type="button" onClick={this.onInsertRow.bind(this, model.rows.size)}
+          disabled={!editMode}
+          className="btn btn-link">+ Add row</button>
+        <button type="button"
+          disabled={!editMode}
+          onClick={this.onInsertColumn.bind(this, model.rows.size)}
+          className="btn btn-link">+ Add column</button>
+      </div>
+    );
+  }
+
+}
+
