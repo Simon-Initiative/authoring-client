c5e5f1de3c77f3575861d8430bfb9fff983555e6
Darren Siegel <siegel.darren@gmail.com>
2018-06-07 15:23:50 -0400

    allow file linking via media library

--- src/editors/content/learning/contiguoustext/ContiguousTextToolbar.tsx
+++ src/editors/content/learning/contiguoustext/ContiguousTextToolbar.tsx
@@ -227,8 +227,8 @@ export default class ContiguousTextToolbar
               }
             }
             disabled={!supports('link') || !rangeEntitiesEnabled}
-            tooltip="External Hyperlink">
-            <i className={'fa fa-external-link'} />
+            tooltip="Hyperlink">
+            <i className={'fa fa-link'} />
           </ToolbarButton>
           <ToolbarButton
             onClick={
