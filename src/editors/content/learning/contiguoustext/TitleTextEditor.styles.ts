0463c432b1271777a064b9a4822b0624ec81129a
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2018-06-21 09:53:51 -0400

    change styles

--- src/editors/content/learning/contiguoustext/TitleTextEditor.styles.ts
+++ src/editors/content/learning/contiguoustext/TitleTextEditor.styles.ts
@@ -4,31 +4,23 @@ import { JSSStyles } from 'styles/jss';
 export const styles: JSSStyles = {
   titleTextEditor: {
     position: 'relative',
-    padding: '6px',
-    border: '2px solid #f3f3f4',
-    transition: 'all .2s ease-in-out',
-    borderRadius: 4,
 
-    '&:hover': {
-      backgroundColor: 'white',
-    },
-    '&:focus': {
-      backgroundColor: 'white',
-      borderColor: 'rgb(76, 154, 255)',
-    },
-    '&:active': {
-      backgroundColor: 'white',
-      borderColor: 'rgb(76, 154, 255)',
-    },
   },
   contiguousTextEditor: {
+    transition: 'all .2s ease-in-out',
+    padding: '6px',
     // backgroundColor: 'transparent',
-    '&:focus': {
-      backgroundColor: 'white',
+    backgroundColor: 'white',
+    border: '2px solid #f3f3f4',
+    borderRadius: 5,
+
+    // padding: 6,
+    '&:hover': {
+      // backgroundColor: 'white',
       borderColor: 'rgb(76, 154, 255)',
     },
     '&:active': {
-      backgroundColor: 'white',
+      // backgroundColor: 'white',
       borderColor: 'rgb(76, 154, 255)',
     },
   },
