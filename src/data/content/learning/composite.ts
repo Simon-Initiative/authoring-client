6c1878c090bacc72283201e3a6f41585d13cf64f
Ben Orgera <benorg@me.com>
2018-07-24 08:22:33 -0400

    fixed contiguous text p (#357)

--- src/data/content/learning/composite.ts
+++ src/data/content/learning/composite.ts
@@ -99,9 +99,13 @@ export class Composite extends Immutable.Record(defaultContent) {
     this.title.lift(title => optional.push(title.toPersistence()));
     this.instructions.lift(i => optional.push(i.toPersistence()));
 
-    const required = this.content.content.size === 0
-      ? [{ p: { '#text': ' ' } }]
-      : this.content.toPersistence();
+    const encoded = this.content.toPersistence();
+    const required = encoded.length === 0 ? [{
+      p: {
+        '#text': ' ',
+        '@id': createGuid(),
+      },
+    }] : encoded;
 
     const s = {
       composite_activity: {
