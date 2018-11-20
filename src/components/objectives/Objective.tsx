fd98fd3bbb0c190ad5d64aa8f92edb8c84b97a92
Eli Knebel <eliknebel@cmu.edu>
2018-11-19 19:37:35 -0500

    add assessment grid to objective details

--- src/components/objectives/Objective.tsx
+++ src/components/objectives/Objective.tsx
@@ -14,6 +14,9 @@ import { InlineEdit } from './InlineEdit';
 import { CourseModel } from 'data/models';
 import { stringFormat } from 'utils/format';
 import flatui from 'styles/palettes/flatui';
+import history from 'utils/history';
+
+const SKILL_GRID_HEADER_HEIGHT = 180;
 
 export const styles: JSSStyles = {
   Objective: {
@@ -30,7 +33,7 @@ export const styles: JSSStyles = {
     flexDirection: 'row',
     fontSize: '1.2em',
     minHeight: 50,
-    padding: [10, 0],
+    padding: [10, 0, 20, 0],
     cursor: 'pointer',
 
     '&:hover': {
@@ -42,9 +45,9 @@ export const styles: JSSStyles = {
     flex: 1,
     marginLeft: 10,
   },
-  details: {
+  detailsQuad: {
     marginLeft: 20,
-    paddingBottom: 10,
+    paddingBottom: 20,
 
     '& h3': {
       display: 'flex',
@@ -53,6 +56,64 @@ export const styles: JSSStyles = {
       fontWeight: 600,
       marginTop: 10,
     },
+  },
+  quadTop: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  quadBottom: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  quadLeft: {
+    flex: 1,
+  },
+  quadRight: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  skillGridHeader: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+  },
+  skillGrid: {
+    margin: [0, 20, 20, 0],
+    borderLeft: 'none',
+  },
+  skillGridRow: {
+    height: 35,
+    borderTop: [1, 'solid', colors.grayLighter],
+    marginRight: (SKILL_GRID_HEADER_HEIGHT - 20),
+
+    '&:nth-child(even)': {
+      backgroundColor: '#F7FBFF',
+    },
+
+    '&:last-child': {
+      borderBottom: [1, 'solid', colors.grayLighter],
+      height: 37,
+    },
+  },
+  skillGridCell: {
+    display: 'inline-block',
+    padding: 4,
+    borderRight: [1, 'solid', colors.grayLighter],
+    width: 35,
+    textAlign: 'center',
+    height: 35,
+    position: 'relative',
+  },
+  gridAssessmentIcon: {
+    position: 'absolute',
+    top: 9,
+    left: 9,
+  },
+  formativeColor: {
+    color: flatui.amethyst,
+  },
+  summativeColor: {
+    color: flatui.nephritis,
   },
   detailsSectionIcon: {
     marginRight: 5,
@@ -85,21 +146,27 @@ export const styles: JSSStyles = {
     },
   },
   pageList: {
-    margin: [10, 20],
+    margin: [10, 0, 80, 20],
   },
   skillList: {
-    margin: [10, 20],
+    margin: [0, 0, 20, 20],
     border: [1, 'solid', colors.grayLighter],
   },
-  noItemsMsg: {
+  noPagesMsg: {
     color: colors.grayDark,
-    margin: 20,
+    margin: [20, 0, 80, 20],
+  },
+  noSkillsMsg: {
+    color: colors.grayDark,
+    margin: [20, 0, 20, 20],
   },
   skill: {
     display: 'flex',
     flexDirection: 'row',
     padding: 4,
     borderBottom: [1, 'solid', colors.grayLighter],
+    height: 35,
+    overflow: 'hidden',
 
     '&:nth-child(even)': {
       backgroundColor: '#F7FBFF',
@@ -126,6 +193,15 @@ export const styles: JSSStyles = {
     lineHeight: 1,
     padding: 0,
     marginLeft: 20,
+  },
+  assessmentLink: {
+    fill: colors.primary,
+    cursor: 'pointer',
+
+    '&:hover': {
+      fill: colors.hover,
+      textDecoration: 'underline',
+    },
   },
   loading: {
     color: colors.gray,
@@ -188,12 +264,6 @@ export class Objective
   }
 
   componentDidMount() {
-    const { isExpanded } = this.props;
-
-    // if (isExpanded) {
-    //   this.loadReferences();
-    // }
-
     this.loadReferences();
   }
 
@@ -252,6 +322,118 @@ export class Objective
 
   onSkillRemove(model: contentTypes.Skill) {
     this.props.onRemoveSkill(model);
+  }
+
+  getOrderedObjectiveAssessments = () => {
+    const { course, skills, skillFormativeRefs, skillSummativeRefs } = this.props;
+
+    const assessmentIdRefs: List<string> = skills.reduce(
+      (acc, skill) => acc.concat(skillFormativeRefs
+        .valueOr(Map<string, List<string>>())
+        .get(skill.id))
+        .concat(skillSummativeRefs
+        .valueOr(Map<string, List<string>>())
+        .get(skill.id)).toList(),
+      List<string>(),
+    ).filter(ref => !!ref).sort((a, b) => a.localeCompare(b)).toList();
+
+    return assessmentIdRefs.map(ref => course.resourcesById.get(ref)).toArray();
+  }
+
+  renderSkillGridHeader() {
+    const { classes, course } = this.props;
+
+    const LEFT_OFFSET = 20;
+    const diagonalDist = (height: number) => Math.sqrt(2 * (height * height));
+    const orderedObjectiveAssessments = this.getOrderedObjectiveAssessments();
+
+    return orderedObjectiveAssessments.length > 0
+      ? (
+        <div className={classes.skillGridHeader}>
+          <div className="flex-spacer"/>
+          <div style={{ height: SKILL_GRID_HEADER_HEIGHT }}>
+            <svg width="100%" height={SKILL_GRID_HEADER_HEIGHT} viewBox="0 0">
+              <g transform={`translate(-15, ${SKILL_GRID_HEADER_HEIGHT}) rotate(-45)`}>
+                <line
+                  x1={10}
+                  y1={10}
+                  x2={diagonalDist(SKILL_GRID_HEADER_HEIGHT)}
+                  y2={10}
+                  stroke={colors.grayLighter} />
+              </g>
+              <g transform={`translate(${SKILL_GRID_HEADER_HEIGHT - 8}, 7)`}>
+                <line
+                  x1={0}
+                  y1={0}
+                  x2={35 * orderedObjectiveAssessments.length}
+                  y2={0}
+                  stroke={colors.grayLighter} />
+              </g>
+              {orderedObjectiveAssessments
+                .map((assessment, i) => (
+                  <g key={assessment.guid}
+                    transform={`translate(${LEFT_OFFSET + (35 * i)}, `
+                      + `${SKILL_GRID_HEADER_HEIGHT}) rotate(-45)`}>
+                    <text
+                      className={classes.assessmentLink}
+                      transform="translate(10, 2)"
+                      onClick={() => history.push(`/${assessment.guid}-${course.guid}`)}>
+                      {stringFormat.ellipsize(assessment.title, 24)}
+                    </text>
+                    <line
+                      x1={10}
+                      y1={10}
+                      x2={diagonalDist(SKILL_GRID_HEADER_HEIGHT)}
+                      y2={10}
+                      stroke={colors.grayLighter} />
+                  </g>
+                ))}
+            </svg>
+          </div>
+        </div>
+      )
+      : null;
+  }
+
+  renderSkillGrid() {
+    const { classes, skills, skillFormativeRefs , skillSummativeRefs} = this.props;
+
+    const orderedObjectiveAssessments = this.getOrderedObjectiveAssessments();
+    const skillContainsFormative = (skill: contentTypes.Skill, formative: contentTypes.Resource) =>
+      skillFormativeRefs.valueOr(Map<string, List<string>>()).has(skill.id)
+      && skillFormativeRefs.valueOr(Map<string, List<string>>()).get(skill.id)
+        .contains(formative.id);
+    const skillContainsSummative = (skill: contentTypes.Skill, summative: contentTypes.Resource) =>
+    skillSummativeRefs.valueOr(Map<string, List<string>>()).has(skill.id)
+      && skillSummativeRefs.valueOr(Map<string, List<string>>()).get(skill.id)
+        .contains(summative.id);
+
+    return orderedObjectiveAssessments.length > 0
+      ? (
+        <div className={classes.skillGrid}>
+          {skills.toArray().map((skill, i) => (
+            <div key={skill.guid} className={classes.skillGridRow}>
+              {orderedObjectiveAssessments.map((assessment, j) => (
+                <div key={assessment.guid} className={classes.skillGridCell}>
+                  {skillContainsFormative(skill, assessment)
+                    ? (
+                      <i className={classNames(['fa fa-flask',
+                        classes.formativeColor, classes.gridAssessmentIcon])} />
+                    )
+                    : skillContainsSummative(skill, assessment)
+                      ? (
+                        <i className={classNames(['fa fa-check',
+                          classes.summativeColor, classes.gridAssessmentIcon])} />
+                      )
+                      : null
+                  }
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      )
+      : null;
   }
 
   renderSkillActions(skill: contentTypes.Skill) {
@@ -322,6 +504,7 @@ export class Objective
       </React.Fragment>
     );
   }
+
   renderSkills() {
     const {
       classes, skills, editMode, loading, onEditSkill,
@@ -371,103 +554,119 @@ export class Objective
       });
 
     const REF_LIMIT = 10;
+    const RIGHT_QUAD_WIDTH = (this.getOrderedObjectiveAssessments().length * 35)
+      + (SKILL_GRID_HEADER_HEIGHT);
 
     return (
-      <div className={classes.details}>
-        <h3>
-        <i className={classNames(['fa fa-file-o', classes.detailsSectionIcon])} />
-        Pages
-        {workbookPageRefs.caseOf({
-          just: refs => (
-            <span className={classNames(['badge badge-light', classes.countBadge])}>
-              {refs.size}
-            </span>
-          ),
-          nothing: () => null,
-        })}
-        </h3>
-        {workbookPageRefs.caseOf({
-          just: (refs) => {
-            return refs.size > 0
-            ? (
-              <div className={classes.pageList}>
-                {refs.slice(0, REF_LIMIT).map(refGuid => (
-                  <div key={refGuid}>
-                    <a href={`./#${getRefGuidFromRefId(refGuid)}-${course.guid}`}>
-                    <i className={classNames(['fa fa-file-o', classes.detailsSectionIcon])} />
-                    {' ' + stringFormat.ellipsize(getWBPTitleFromRefId(refGuid), 100, 5)}
-                    </a>
+      <div className={classes.detailsQuad}>
+        <div className={classes.quadTop}>
+          <div className={classes.quadLeft}>
+            <h3>
+            <i className={classNames(['fa fa-file-o', classes.detailsSectionIcon])} />
+            Pages
+            {workbookPageRefs.caseOf({
+              just: refs => (
+                <span className={classNames(['badge badge-light', classes.countBadge])}>
+                  {refs.size}
+                </span>
+              ),
+              nothing: () => null,
+            })}
+            </h3>
+            {workbookPageRefs.caseOf({
+              just: (refs) => {
+                return refs.size > 0
+                ? (
+                  <div className={classes.pageList}>
+                    {refs.slice(0, REF_LIMIT).map(refGuid => (
+                      <div key={refGuid}>
+                        <a href={`./#${getRefGuidFromRefId(refGuid)}-${course.guid}`}>
+                        <i className={classNames(['fa fa-file-o', classes.detailsSectionIcon])} />
+                        {stringFormat.ellipsize(getWBPTitleFromRefId(refGuid), 100, 5)}
+                        </a>
+                      </div>
+                    ))}
+                    {refs.size > REF_LIMIT
+                      ? (
+                        <div>and {refs.size - REF_LIMIT} more...</div>
+                      )
+                      : null
+                    }
                   </div>
-                ))}
-                {refs.size > REF_LIMIT
-                  ? (
-                    <div>and {refs.size - REF_LIMIT} more...</div>
-                  )
-                  : null
-                }
+                )
+                : (
+                  <div className={classes.noPagesMsg}>
+                    <span>This objective is not referenced by any workbook pages.</span>
+                  </div>
+                );
+              },
+              nothing: () => (
+              <div className={classes.loading}>
+                <i className="fa fa-circle-o-notch fa-spin fa-fw"/> Loading...
               </div>
-            )
-            : (
-              <div className={classes.noItemsMsg}>
-                <span>This objective is not referenced by any workbook pages.</span>
-              </div>
-            );
-          },
-          nothing: () => (
-          <div className={classes.loading}>
-            <i className="fa fa-circle-o-notch fa-spin fa-fw"/> Loading...
+              ),
+            })}
+            <h3>
+              <i className={classNames(['fa fa-cubes', classes.detailsSectionIcon])} />
+              Skills
+              <span className={classNames(['badge badge-light', classes.countBadge])}>
+                {skills.size}
+              </span>
+              <Button
+                className={classes.addSkillButton}
+                editMode={editMode && !loading}
+                type="link"
+                onClick={() => onAddExistingSkill(objective)}>
+                Add Existing Skill
+              </Button>
+              <Button
+                className={classes.addSkillButton}
+                editMode={editMode && !loading}
+                type="link"
+                onClick={() => onAddNewSkill(objective)}>
+                Create New Skill
+              </Button>
+            </h3>
           </div>
-          ),
-        })}
-        <h3>
-        <i className={classNames(['fa fa-cubes', classes.detailsSectionIcon])} />
-        Skills
-        <span className={classNames(['badge badge-light', classes.countBadge])}>
-          {skills.size}
-        </span>
-          <Button
-            className={classes.addSkillButton}
-            editMode={editMode && !loading}
-            type="link"
-            onClick={() => onAddExistingSkill(objective)}>
-            Add Existing Skill
-          </Button>
-          <Button
-            className={classes.addSkillButton}
-            editMode={editMode && !loading}
-            type="link"
-            onClick={() => onAddNewSkill(objective)}>
-            Create New Skill
-          </Button>
-        </h3>
-          {skills.size > 0
-            ? (
-              <div className={classes.skillList}>
-                {this.renderSkills()}
-              </div>
-            )
-            : (
-              <div className={classes.noItemsMsg}>
-                <span>No skills are assigned to this objective. You should </span>
-                <Button
-                  className={classNames([classes.addSkillButton])}
-                  editMode={editMode && !loading}
-                  type="inline-link"
-                  onClick={() => onAddExistingSkill(objective)}>
-                  add
-                </Button>
-                <span> or </span>
-                <Button
-                  className={classNames([classes.addSkillButton])}
-                  editMode={editMode && !loading}
-                  type="inline-link"
-                  onClick={() => onAddNewSkill(objective)}>
-                  create
-                </Button>
-                <span> some.</span>
-              </div>
-            )
-          }
+          <div className={classes.quadRight} style={{ width: RIGHT_QUAD_WIDTH }}>
+            {this.renderSkillGridHeader()}
+          </div>
+        </div>
+        <div className={classes.quadBottom}>
+          <div className={classes.quadLeft}>
+            {skills.size > 0
+              ? (
+                <div className={classes.skillList}>
+                  {this.renderSkills()}
+                </div>
+              )
+              : (
+                <div className={classes.noSkillsMsg}>
+                  <span>No skills are assigned to this objective. You should </span>
+                  <Button
+                    className={classNames([classes.addSkillButton])}
+                    editMode={editMode && !loading}
+                    type="inline-link"
+                    onClick={() => onAddExistingSkill(objective)}>
+                    add
+                  </Button>
+                  <span> or </span>
+                  <Button
+                    className={classNames([classes.addSkillButton])}
+                    editMode={editMode && !loading}
+                    type="inline-link"
+                    onClick={() => onAddNewSkill(objective)}>
+                    create
+                  </Button>
+                  <span> some.</span>
+                </div>
+              )
+            }
+          </div>
+          <div className={classes.quadRight} style={{ width: RIGHT_QUAD_WIDTH }}>
+            {this.renderSkillGrid()}
+          </div>
+        </div>
       </div>
     );
   }
