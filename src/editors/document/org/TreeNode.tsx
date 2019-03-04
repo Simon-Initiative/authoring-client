16dfe59bfbfe7d0c5b0d0555235ec2f6202bdda1
Darren Siegel <siegel.darren@gmail.com>
2019-03-04 10:16:41 -0500

    Authoring 1737 cleanup (#516)
    * cleanup style and remove dead code
    
    * styling
    
    * make global style for content specific to treenode
    center text in tree node
    fix flex issue in nav panel

--- src/editors/document/org/TreeNode.tsx
+++ src/editors/document/org/TreeNode.tsx
@@ -2,12 +2,7 @@ import * as React from 'react';
 import * as contentTypes from '../../../data/contentTypes';
 import { AppContext } from '../../common/AppContext';
 import * as models from '../../../data/models';
-import { renderDropTarget } from '../../content/org/drag/utils';
-import { DragHandle } from 'components/common/DragHandle';
-import { DraggableNode } from './DraggableNode';
 import { NodeTypes } from './traversal';
-import { canHandleDrop } from './utils';
-import { Caption } from './Caption';
 import * as org from 'data/models/utils/org';
 
 import './TreeNode.scss';
@@ -32,31 +27,15 @@ export interface TreeNodeProps {
 }
 
 export interface TreeNodeState {
-  mouseOver: boolean;
-}
 
-// tslint:disable-next-line
-const Title = (props) => {
-  return (
-    <button onClick={() => props.toggleExpanded()}
-      type="button" className="btn btn-link">{props.children}</button>
-  );
-};
+}
 
 
 export class TreeNode
   extends React.PureComponent<TreeNodeProps, TreeNodeState> {
-  timer: any;
 
   constructor(props) {
     super(props);
-
-    this.timer = null;
-
-    this.onEnter = this.onEnter.bind(this);
-    this.onLeave = this.onLeave.bind(this);
-
-    this.state = { mouseOver: false };
   }
 
   getLabel(contentType: string) {
@@ -68,19 +47,6 @@ export class TreeNode
     return this.props.labels[contentType.toLowerCase()];
   }
 
-
-  onEnter() {
-    this.setState({ mouseOver: true });
-  }
-
-  onLeave() {
-    if (this.timer !== null) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-    this.setState({ mouseOver: false });
-  }
-
   getAdaptiveNumber() {
     if (this.props.parentModel.contentType !== contentTypes.OrganizationContentTypes.Section) {
       return this.props.numberAtLevel;
@@ -89,98 +55,51 @@ export class TreeNode
     return '';
   }
 
-  render(): JSX.Element {
+  renderTitle() {
 
-    const { model, parentModel, indexWithinParent, isSelected, highlighted,
-      depth, editMode, onReposition, isExpanded } = this.props;
-
-    const hasHiddenChildren =
-      <span>
-        <i className="toggle fa fa-caret-right"></i>
-      </span>;
-
-    const hasShownChildren =
-      <span>
-        <i className="toggle fa fa-caret-down"></i>
-      </span>;
-
-    const icon = isExpanded ? hasShownChildren : hasHiddenChildren;
-
-    const contentType = this.getLabel(this.props.model.contentType);
-
-
-    let titleLabel;
+    const { isExpanded } = this.props;
 
     if (this.props.model.contentType === contentTypes.OrganizationContentTypes.Item) {
 
       const resource = this.props.context.courseModel.resourcesById.get(
         this.props.model.resourceref.idref);
-
-      const titleString = resource !== undefined
+      return resource !== undefined
         ? resource.title
         : 'Loading...';
-
-      titleLabel = <span className="title-label">{titleString}</span>;
-
-    } else if (this.props.model.contentType === contentTypes.OrganizationContentTypes.Include) {
-      titleLabel = 'Include';
-    } else {
-      const number = this.getAdaptiveNumber();
-      titleLabel = <span className="title-label">
-        {icon}{contentType} {number}: {this.props.model.title}
-      </span>;
+    }
+    if (this.props.model.contentType === contentTypes.OrganizationContentTypes.Include) {
+      return 'Include';
     }
 
-    const title = (
-      <Caption
-        onClick={() => this.props.onClick(this.props.model)}
-        depth={0}
-        org={this.props.org} context={this.props.context}
-        isHoveredOver={this.state.mouseOver}
-        editMode={this.props.editMode}
-        isSelected={this.props.isSelected}
-        model={this.props.model}>
-        {titleLabel}
-      </Caption>
-    );
+    const hasHiddenChildren = <i className="toggle fa fa-caret-right"></i>;
+    const hasShownChildren = <i className="toggle fa fa-caret-down"></i>;
 
-    const finalDropTarget =
-      (indexWithinParent === parentModel.children.size - 1)
-        ? renderDropTarget(
-          indexWithinParent + 1, parentModel,
-          canHandleDrop, onReposition, '')
-        : null;
+    const toggle = isExpanded ? hasShownChildren : hasHiddenChildren;
+    const contentType = this.getLabel(this.props.model.contentType);
+    const number = this.getAdaptiveNumber();
 
     return (
+      <React.Fragment>
+        {toggle}{contentType} {number}: {this.props.model.title}
+      </React.Fragment>
+    );
+  }
+
+  render(): JSX.Element {
+
+    const { highlighted, isSelected, depth, model } = this.props;
+    return (
       <tr
-        className={`tree-node ${highlighted ? 'highlighted' : ''} ${isSelected ? 'selected' : ''}`}
-        key={model.guid}
-        onMouseEnter={this.onEnter} onMouseLeave={this.onLeave}>
-        <td className="content">
-          <div className="top-drop">
-            {renderDropTarget(
-              indexWithinParent, parentModel,
-              canHandleDrop, onReposition, model.guid)}
-          </div>
-
-          <div className="node">
-            <DraggableNode
-              id={model.guid}
-              editMode={editMode}
-              index={indexWithinParent}
-              source={model}
-              parentModel={parentModel}>
-              <span style={{ marginLeft: (depth * 20) }} />
-              <DragHandle hidden={!editMode || !this.state.mouseOver} />
-              {title}
-            </DraggableNode>
-          </div>
-
-          <div className="bottom-drop">
-            {finalDropTarget}
+        className={`${highlighted ? 'table-info' : ''}`}
+        key={model.guid}>
+        <td onClick={() => this.props.onClick(model)}>
+          <div className={`treenode-content ${isSelected ? 'selected' : ''}`}>
+            <span className="treenode-title" style={{ marginLeft: (depth * 20) }}>
+              {this.renderTitle()}
+            </span>
           </div>
         </td>
-      </tr>
+      </tr >
     );
   }
 
