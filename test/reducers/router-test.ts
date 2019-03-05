e33ce44e62254dd31732fe078199a433c6d09430
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2019-03-05 15:23:24 -0500

    Fix routing test to check orgId

--- test/reducers/router-test.ts
+++ test/reducers/router-test.ts
@@ -7,7 +7,7 @@ import { router, RouterState } from 'reducers/router';
 describe('router reducer', () => {
 
   it('should update route', () => {
-    const path = '2c92808a66ef8e6b0166f54c5eea0000-2c92808866d069100166d0a014a70006';
+    const path = 'resourceId-courseId-orgId';
     const search = '';
 
     const initialState = new RouterState();
@@ -15,15 +15,33 @@ describe('router reducer', () => {
 
     expect(updatedState.route).toEqual(ROUTE.RESOURCE);
     expect(updatedState.path)
-      .toEqual('2c92808a66ef8e6b0166f54c5eea0000-2c92808866d069100166d0a014a70006');
+      .toEqual('resourceId-courseId-orgId');
     expect(updatedState.search).toEqual('');
-    expect(updatedState.courseId.valueOr('NOTHING')).toEqual('2c92808866d069100166d0a014a70006');
-    expect(updatedState.resourceId.valueOr('NOTHING')).toEqual('2c92808a66ef8e6b0166f54c5eea0000');
+    expect(updatedState.resourceId.valueOr('NOTHING')).toEqual('resourceId');
+    expect(updatedState.courseId.valueOr('NOTHING')).toEqual('courseId');
+    expect(updatedState.orgId.valueOr('NOTHING')).toEqual('orgId');
+    expect(updatedState.urlParams.toJS()).toEqual({});
+  });
+
+  it('should update route with hyphens in resource ID', () => {
+    const path = 'resource-Id-dashes-courseId-orgId';
+    const search = '';
+
+    const initialState = new RouterState();
+    const updatedState = router(initialState, updateRoute(path, search));
+
+    expect(updatedState.route).toEqual(ROUTE.RESOURCE);
+    expect(updatedState.path)
+      .toEqual('resource-Id-dashes-courseId-orgId');
+    expect(updatedState.search).toEqual('');
+    expect(updatedState.resourceId.valueOr('NOTHING')).toEqual('resource-Id-dashes');
+    expect(updatedState.courseId.valueOr('NOTHING')).toEqual('courseId');
+    expect(updatedState.orgId.valueOr('NOTHING')).toEqual('orgId');
     expect(updatedState.urlParams.toJS()).toEqual({});
   });
 
   it('should update route with params', () => {
-    const path = '2c92808a66ef8e6b0166f54c5eea0000-2c92808866d069100166d0a014a70006';
+    const path = 'resourceId-courseId-orgId';
     const search = '?questionId=some_question_id&filter=id';
 
     const initialState = new RouterState();
@@ -31,10 +49,11 @@ describe('router reducer', () => {
 
     expect(updatedState.route).toEqual(ROUTE.RESOURCE);
     expect(updatedState.path)
-      .toEqual('2c92808a66ef8e6b0166f54c5eea0000-2c92808866d069100166d0a014a70006');
+      .toEqual('resourceId-courseId-orgId');
     expect(updatedState.search).toEqual('?questionId=some_question_id&filter=id');
-    expect(updatedState.courseId.valueOr('NOTHING')).toEqual('2c92808866d069100166d0a014a70006');
-    expect(updatedState.resourceId.valueOr('NOTHING')).toEqual('2c92808a66ef8e6b0166f54c5eea0000');
+    expect(updatedState.resourceId.valueOr('NOTHING')).toEqual('resourceId');
+    expect(updatedState.courseId.valueOr('NOTHING')).toEqual('courseId');
+    expect(updatedState.orgId.valueOr('NOTHING')).toEqual('orgId');
     expect(updatedState.urlParams.toJS()).toEqual({ questionId: 'some_question_id', filter: 'id' });
   });
 
