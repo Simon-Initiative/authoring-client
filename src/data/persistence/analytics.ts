67d65f2a8f11c327ecaf38889a1d2b7330ed6534
Darren Siegel <siegel.darren@gmail.com>
2019-04-04 12:26:37 -0400

    the other two endpoints

--- src/data/persistence/analytics.ts
+++ src/data/persistence/analytics.ts
@@ -11,3 +11,29 @@ export function fetchDataSet(dataSetId: string): Promise<Object> {
       return json;
     });
 }
+
+
+export function getAllDataSets(courseId: string): Promise<Object[]> {
+
+  const url = `${configuration.baseUrl}/analytics/${courseId}`;
+  const method = 'GET';
+
+  return (authenticatedFetch({ url, method }) as any)
+    .then((json) => {
+      return json;
+    });
+}
+
+
+
+export function createDataSet(courseId: string): Promise<Object> {
+
+  const url = `${configuration.baseUrl}/analytics/${courseId}`;
+  const method = 'POST';
+
+  return (authenticatedFetch({ url, method }) as any)
+    .then((json) => {
+      return json;
+    });
+}
+
