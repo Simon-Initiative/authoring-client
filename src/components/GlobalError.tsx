c7ed3848f124be70f1fae70138779050b8b5740f
Darren Siegel <siegel.darren@gmail.com>
2019-04-23 10:20:41 -0400

    publish errors to ga

--- src/components/GlobalError.tsx
+++ src/components/GlobalError.tsx
@@ -1,8 +1,11 @@
+import { useState, useEffect } from 'react';
+// tslint:disable-next-line
 import * as React from 'react';
 import { withStyles, classNames } from 'styles/jss';
 import { StyledComponentProps } from 'types/component';
 import { styles } from './GlobalError.styles';
 import { buildFeedbackFromCurrent } from 'utils/feedback';
+import { checkPropTypes } from 'prop-types';
 
 export interface GlobalErrorProps {
   error: any;
@@ -12,47 +15,56 @@ export interface GlobalErrorProps {
 }
 
 const GlobalError:
-React.StatelessComponent<StyledComponentProps<GlobalErrorProps, typeof styles>> = ({
-  classes, userName, email,
-}) => {
+  React.StatelessComponent<StyledComponentProps<GlobalErrorProps, typeof styles>> = ({
+    classes, userName, email, error,
+  }) => {
 
-  const reportError = () => {
-    const url = buildFeedbackFromCurrent(userName, email);
-    window.open(url, 'error');
-  };
+    const [reported, setReported] = useState(false);
+    useEffect(() => {
+      // Report this error event to google analytics
+      if (!reported) {
+        (window as any).gtag('event', 'exception', { description: error, fatal: true });
+        setReported(true);
+      }
+    });
 
-  return (
-    <div className={classNames([classes.globalError])}>
+    const reportError = () => {
+      const url = buildFeedbackFromCurrent(userName, email);
+      window.open(url, 'error');
+    };
 
-      <div className={classNames([classes.globalContent])}>
+    return (
+      <div className={classNames([classes.globalError])}>
 
-        <div>
-          <span>
-            <i className="far fa-frown fa-4x"></i>
-          </span>
-        </div>
+        <div className={classNames([classes.globalContent])}>
 
-        <p>
-          Oh no! Something went wrong.
+          <div>
+            <span>
+              <i className="far fa-frown fa-4x"></i>
+            </span>
+          </div>
+
+          <p>
+            Oh no! Something went wrong.
         </p>
 
-        <p>
-          Refreshing your browser and trying again may
-          fix the problem.
+          <p>
+            Refreshing your browser and trying again may
+            fix the problem.
         </p>
 
-        <button
-          onClick={reportError}
-          className="btn btn-primary">
-          Report this problem
+          <button
+            onClick={reportError}
+            className="btn btn-primary">
+            Report this problem
         </button>
 
+        </div>
+
       </div>
+    );
 
-    </div>
-  );
-
-};
+  };
 
 const StyledGlobalError = withStyles<GlobalErrorProps>(styles)(GlobalError);
 export { StyledGlobalError as GlobalError };
