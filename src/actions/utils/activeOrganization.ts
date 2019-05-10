dc814f28c1c0456ab9371525fb4de81bd3efbe2a
Zach Bluedorn <zachary.bluedorn@gmail.com>
2019-05-10 09:20:04 -0400

    Change "copy organization" button to switch to new org (#602)
    * Change org duplication to make the new org active
    
    * set version
    
    * Update license and dependencies (#600)
    
    * Add build badges
    
    Add build badges
    
    * seperate devDependencies from main deps
    update license
    
    * linting
    
    * remove unused import
    
    * Add data analytics error message when creation fails (#601)
    
    * add data analytics message
    
    * Add period to wording
    
    * move undo redo toolbar (#603)
    
    * Change background color of likertseries options (#605)

--- src/actions/utils/activeOrganization.ts
+++ src/actions/utils/activeOrganization.ts
@@ -1,3 +1,23 @@
+import { Maybe } from 'tsmonad';
+import { loadFromLocalStorage, saveToLocalStorage } from 'utils/localstorage';
+
 export const ACTIVE_ORG_STORAGE_KEY = 'activeOrganization';
 export const activeOrgUserKey = (username: string, courseGuid: string) =>
   username + ':' + courseGuid;
+
+
+export function updateActiveOrgPref(
+  courseGuid: string, username: string, organizationGuid: string) {
+
+  const userKey = activeOrgUserKey(username, courseGuid);
+
+  Maybe.maybe(loadFromLocalStorage(ACTIVE_ORG_STORAGE_KEY))
+    .caseOf({
+      just: (prefs) => {
+        prefs[userKey] = organizationGuid;
+        saveToLocalStorage(ACTIVE_ORG_STORAGE_KEY, JSON.stringify(prefs));
+      },
+      nothing: () => saveToLocalStorage(
+        ACTIVE_ORG_STORAGE_KEY, JSON.stringify({ [userKey]: organizationGuid })),
+    });
+}
