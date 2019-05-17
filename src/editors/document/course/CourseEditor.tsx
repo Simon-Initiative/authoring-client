05c4003042f4a3ffd36e84e8fa5190edb5dfbd19
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2019-05-17 11:12:38 -0400

    fix typo

--- src/editors/document/course/CourseEditor.tsx
+++ src/editors/document/course/CourseEditor.tsx
@@ -471,7 +471,7 @@ class CourseEditor extends React.Component<CourseEditorProps, CourseEditorState>
         const deploymentStatus = (deployStatusObj as any).deployStatus;
         courseChanged(model.with({ deploymentStatus }));
       })
-      .catch(err => onShowMessage(eralErrorMessage(
+      .catch(err => onShowMessage(buildGeneralErrorMessage(
         `There was an error requesting course deployment: ${err.message}`)));
   }
 
@@ -739,7 +739,7 @@ class CourseEditor extends React.Component<CourseEditorProps, CourseEditorState>
                       <React.Fragment>
                         Analytics for this course are based on the latest dataset, which was created
                       {' '}<b>{dateFormatted(parseDate(dataSet.dateCreated))}</b>.
-                              To get the most recent data for analytics, create a new dataset.
+                                To get the most recent data for analytics, create a new dataset.
                         <br />
                         <br />
                         <b>Notice:</b> Dataset creation may take a few minutes depending on the size
