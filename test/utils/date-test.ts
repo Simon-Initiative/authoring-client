258654d4c1cfebfb0a8794deb7d7d4087b272f26
Kyle Kozlowski <koz@KOLIM.wv.cc.cmu.edu>
2019-06-21 15:40:45 -0400

    relativeTo is now tested and passing

--- test/utils/date-test.ts
+++ test/utils/date-test.ts
@@ -1,4 +1,4 @@
-import { parseDate, compareDates, relativeToNow } from 'utils/date';
+import { parseDate, compareDates, relativeTo } from 'utils/date';
 
 it('parseDate test', () => {
   expect(parseDate('May 5, 2019 3:24:00 AM')).toBe(new Date('2019-5-17T03:24:00'));
@@ -11,21 +11,69 @@ it('compareDates test', () => {
 });
 
 // testing for all 13 possible outputs
-// note: tests depend on the current time for now!  need tp be updated for stablility
-it('relativeToNow test', () => {
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date())).toBe('just now');
-  expect(relativeToNow(new Date('2019-5-17T03:24:00'))).toBe('a month ago');
-  expect(relativeToNow(new Date('2018-5-17T03:24:00'))).toBe('a year ago');
-  expect(relativeToNow(new Date('December 17, 1995 03:24:00'))).toBe('13 years ago');
-
+// takes dates in chronological order as arguments, here expressed in ms
+// numbers calculated using constants from relativeTo function
+// tests should be exactly on the margin between different timestamps
+it('relativeTo test', () => {
+  expect(relativeTo(new Date(),
+                    new Date())).toBe('just now');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000001999))).toBe('just now');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000002000))).toBe('a few seconds ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000006999))).toBe('a few seconds ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000007000))).toBe('7 seconds ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000059999))).toBe('59 seconds ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000060000))).toBe('a minute ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000119999))).toBe('a minute ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500000120000))).toBe('2 minutes ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500003599999))).toBe('59 minutes ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500003600000))).toBe('an hour ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500007199999))).toBe('an hour ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500007200000))).toBe('2 hours ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500086399999))).toBe('23 hours ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500086400000))).toBe('a day ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500172799999))).toBe('a day ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500172800000))).toBe('2 days ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500518399999))).toBe('5 days ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(500518400000))).toBe('a week ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(501209599999))).toBe('a week ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(501209600000))).toBe('2 weeks ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(502635199999))).toBe('4 weeks ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(502635200000))).toBe('a month ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(505270399999))).toBe('a month ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(505270400000))).toBe('2 months ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(528900799999))).toBe('10 months ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(528900800000))).toBe('a year ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(563071999999))).toBe('a year ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date(563072000000))).toBe('2 years ago');
+  expect(relativeTo(new Date(500000000000),
+                    new Date('June 21, 2019 2:24:00 PM'))).toBe('33 years ago');
 
 });
