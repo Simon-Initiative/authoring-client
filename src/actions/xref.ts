e24dd382fc0dde4767214d1c5a6d3c9ac98681da
Darren Siegel <siegel.darren@gmail.com>
2019-07-01 15:39:59 -0400

    Merge down to 31 (#645)
    * Sprint 0.29.0 (#635)
    
    * set version
    
    * include an explicit Provider wrapper (#629)
    
    * Authoring 1910 question analytics (#630)
    
    * refactor analytics to support question level analytics
    
    * add multipart analytics support
    improve analytics on question and move to tab
    
    * allow creation and selection in nested CTEs (#631)
    
    * Authoring 1923 incorrect summative count (#633)
    
    * refactor analytics to support question level analytics
    
    * add multipart analytics support
    improve analytics on question and move to tab
    
    * fix an issue where summative count doesn't include deeply linked pools from workbook pages
    fix an issue where count might be * which results in summative being NaN
    
    * add ui for * option
    
    * remove selection text AUTHORING-1928
    
    * Sprint 0.30.0 (#641)
    
    * new sprint branch
    
    * only include bib if there are entries (#638)
    
    * allow source change (#639)
    
    * Authoring 1850 no modules (#636)
    
    * parameterized the title and message
    
    * added comment on pluralization
    
    * just eliminate the plural problem
    
    * incorporate theme images into ui (#637)
    
    * Add date unit tests (#640)
    
    * have initial tests up, minimum coverage
    
    * cleanup
    
    * relativeTo is now tested and passing
    
    * basic tests up, need to improve thoroughness
    
    * tests up, ready for pull request
    
    * Update package.json
    
    Found this to be the easiest way to update the package file, all jest tests should now run and pass
    
    * revert typescript bump, use toEqual instead
    
    * revert back to jest 23.6
    
    * simplify assert
    
    * Authoring 1967 xref (#644)
    
    * new sprint branch
    
    * only include bib if there are entries (#638)
    
    * allow source change (#639)
    
    * Authoring 1850 no modules (#636)
    
    * parameterized the title and message
    
    * added comment on pluralization
    
    * just eliminate the plural problem
    
    * incorporate theme images into ui (#637)
    
    * Add date unit tests (#640)
    
    * have initial tests up, minimum coverage
    
    * cleanup
    
    * relativeTo is now tested and passing
    
    * basic tests up, need to improve thoroughness
    
    * tests up, ready for pull request
    
    * Update package.json
    
    Found this to be the easiest way to update the package file, all jest tests should now run and pass
    
    * revert typescript bump, use toEqual instead
    
    * revert back to jest 23.6
    
    * simplify assert
    
    * use the id of a page instead of guid
    
    * persist the id, not the guid

--- src/actions/xref.ts
+++ src/actions/xref.ts
@@ -26,9 +26,10 @@ function setTargetNode(node): SetXrefTargetAction {
 
 // targetId is an id
 // documentId is a guid
-export function fetchAndSetTargetNode(targetId: string, documentId: string) {
+export function fetchAndSetTargetNode(targetId: string, resourceId: string) {
   return (dispatch: Dispatch, getState: () => State): Promise<any> => {
     const { course } = getState();
+    const documentId = course.resourcesById.get(resourceId).guid;
     return persistence.retrieveDocument(course.idvers, documentId).then((doc) => {
       const wbpage = doc.model as WorkbookPageModel;
       // Find the target node in the workbook page's content tree
