126d8810f41425c15173ccf70aceb3b220c0b839
Kyle Kozlowski <koz@KOLIM.wv.cc.cmu.edu>
2019-07-11 15:10:08 -0400

    fixed crashing when only one WBP present, deciding what to display instead

--- src/editors/content/learning/XrefEditor.tsx
+++ src/editors/content/learning/XrefEditor.tsx
@@ -62,14 +62,21 @@ export default class XrefEditor
   }
 
   componentDidMount() {
+    console.log('PAGEEEEEEEEEE');
     const { target, model, updateTarget } = this.props;
 
     // Xref must have a page set in order to allow linking to a target element. We set the default
     // page to be the first workbook page so that an error is not thrown in case the user chooses
     // a target element without changing the page using the 'page to link to' dropdown
+    console.log("BEFORE##############%%%%%%%%%%!!!!!");
     if (!this.props.model.page) {
-      this.props.onEdit(this.props.model.with({ page: this.pages[0].guid }));
+      //KYLE-1997 With only one WBP in course, pages array is empty, and pages[0] is undefined
+      this.props.onEdit(this.props.model.with(
+        { page: this.pages[0] ? this.pages[0].guid :  'NO PAGES AVAILABLE' }));
+      console.log('PAGEEEEEEEEEE');
+      console.log(this.props.model.page);
     }
+    console.log("Past buggy part");
 
     // Check if we need to fetch the target element from its workbook page
     if (!target && model.idref) {
@@ -122,18 +129,19 @@ export default class XrefEditor
 
   renderSidebar() {
     const { editMode, model, onEdit, target } = this.props;
-
+    // KYLE-1997 The items displayed in the drop are HTML option elements in the array below
     const pageOptions = this.pages.map(r => <option key={r.guid} value={r.id}>{r.title}</option>);
+    console.table(pageOptions);
 
     return (
-      <SidebarContent title="Cross Reference">
+      <SidebarContent title={model.page}>
         <SidebarGroup label="Page to link to">
           <Select
             editMode={this.props.editMode}
             label=""
-            value={model.page}
+            value="Cross Reference"
             onChange={this.onChangePage}>
-            {pageOptions}
+            {<option key={r.guid} value={r.id}>{r.title}</option>/*pageOptions*/}
           </Select>
         </SidebarGroup>
         <SidebarGroup label="Element to link to">
