8b145b3022e9846c1470bd8084719a70d3d36d26
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2019-11-26 21:17:11 -0500

    fix analytics refresh problem

--- src/editors/document/org/OrgComponentEditor.tsx
+++ src/editors/document/org/OrgComponentEditor.tsx
@@ -83,8 +83,13 @@ export class OrgComponentEditor
   }
 
   componentWillReceiveProps(nextProps: OrgComponentEditorProps) {
+    function isDifferentOrg(thisOrg, nextOrg) {
+      const thisOrgId = thisOrg.valueOr({ resource: {} }).resource.id;
+      return !thisOrgId || thisOrgId !== nextOrg.valueOr({ resource: {} }).resource.id;
+    }
     if (this.props.componentId !== nextProps.componentId
-      || this.props.org !== nextProps.org) {
+      || isDifferentOrg(this.props.org, nextProps.org)) {
+      console.log('Finding component model')
       this.findComponentModel(nextProps);
     }
   }
