9b93fee60ba928a3add293cd6eb6bec7898a9593
Zach Bluedorn <zachary.bluedorn@gmail.com>
2019-12-02 13:24:20 -0500

    0.38.0 - Enable numeric input questions to have multiple correct answers (#729)
    * Add fromPersistence logic to move responses with  to the end, and add unit tests to check it
    
    * change star responses to single
    
    * guard against using first on a list
    
    * change structure of new logic
    
    * add comments
    
    * add authoring number
    
    * fix typo

--- test/data/content/assessment/input-numeric-test.ts
+++ test/data/content/assessment/input-numeric-test.ts
@@ -2,6 +2,8 @@ import { Question } from 'data/content/assessment/question';
 import guid from 'utils/guid';
 import { responsesMatchAnswerChoices, testScores } from './_questions-test';
 const jsonInputNumeric = require('./questions-valid/input-numeric.json');
+const jsonMatchStarValid = require('./questions-valid/input-numeric-match-star.json');
+const jsonMatchStarInvalid = require('./questions-invalid/input-numeric-match-star.json');
 
 // tslint:disable max-line-length
 describe('input - numeric should be parsed correctly', () => {
@@ -43,4 +45,34 @@ describe('input - numeric should be parsed correctly', () => {
   });
 
   testScores(responses);
+});
+
+
+describe('input - numeric have `match: "*" responses only at the end`', () => {
+  // This question has a response with match "*" in the wrong position, and needs a change
+  const invalidQuestionStarMatch: Question = Question.fromPersistence(
+    Question.fromPersistence(jsonMatchStarInvalid, guid()).toPersistence(),
+    guid());
+
+  // This question has the match "*" in the correct position, and needs no change
+  const validQuestionStarMatch: Question = Question.fromPersistence(
+    Question.fromPersistence(jsonMatchStarValid, guid()).toPersistence(),
+    guid());
+
+  it('makes no changes to the responses when a `match: "*"` is at the end', () => {
+    expect(validQuestionStarMatch.toPersistence()).toEqual(jsonMatchStarValid);
+  });
+
+  it('makes a change to the responses when a `match: "*" is not at the end', () => {
+    expect(invalidQuestionStarMatch.toPersistence()).not.toEqual(jsonMatchStarInvalid);
+  });
+
+  it('does not allow responses to have a `match: "*"` except for at the end', () => {
+    expect(invalidQuestionStarMatch.parts.every(part =>
+      part.responses.butLast().every(response => response.match !== '*'))).not.toBe(false);
+  });
+
+  it('ensures the `match: "*"` stays at the end for valid questions', () => {
+    expect(invalidQuestionStarMatch.parts.some(part => part.responses.last().match === '*')).toBe(true);
+  });
 });
