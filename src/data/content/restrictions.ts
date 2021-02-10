86280cfb9ed410ae6573ed5ff4d24d6a23a29a99
Darren Siegel <siegel.darren@gmail.com>
2021-02-10 13:52:39 -0500

    fix citation tool

--- src/data/content/restrictions.ts
+++ src/data/content/restrictions.ts
@@ -1,19 +1,5 @@
 export const nonMoveableTypes = {
-  Article: true,
-  Book: true,
-  Booklet: true,
-  Conference: true,
-  InBook: true,
-  InCollection: true,
-  InProceedings: true,
-  Manual: true,
-  MastersThesis: true,
-  PhdThesis: true,
-  Proceedings: true,
-  TechReport: true,
-  Misc: true,
-  Unpublished: true,
-  RichText: true,
+
 };
 
 export const nonRemovableTypes = {
