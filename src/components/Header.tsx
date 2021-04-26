51ccc9f267dfdae603fe0f9ba1459f67de657ad2
Anders Weinstein <andersw@cs.cmu.edu>
2021-04-26 07:33:23 -0400

    Use 'OLI Echo' in app header and browser tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,45 @@
-import * as React from 'react';
-import { CourseModel } from 'data/models';
-import { UserState } from 'reducers/user';
-import { Maybe } from 'tsmonad';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import { buildFeedbackFromCurrent } from 'utils/feedback';
-import { getVersion } from 'utils/buildinfo';
-import { StyledComponentProps } from 'types/component';
-import { withStyles, classNames, JSSStyles } from 'styles/jss';
-import colors from 'styles/colors';
-import * as chroma from 'chroma-js';
-import { RouterState } from 'reducers/router';
-import * as viewActions from 'actions/view';
+import * as React from "react";
+import { CourseModel } from "data/models";
+import { UserState } from "reducers/user";
+import { Maybe } from "tsmonad";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { buildFeedbackFromCurrent } from "utils/feedback";
+import { getVersion } from "utils/buildinfo";
+import { StyledComponentProps } from "types/component";
+import { withStyles, classNames, JSSStyles } from "styles/jss";
+import colors from "styles/colors";
+import * as chroma from "chroma-js";
+import { RouterState } from "reducers/router";
+import * as viewActions from "actions/view";
 
-const OLI_ICON = require('../../assets/oli-icon.png');
+const OLI_ICON = require("../../assets/oli-icon.png");
 
 const styles: JSSStyles = {
   Header: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'row',
-    width: '100%',
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "100%",
     height: 50,
     padding: [5, 30],
     color: colors.white,
     backgroundColor: colors.primary,
-    borderBottom: [3, 'solid', chroma(colors.primary).darken(0.4).hex()],
-    verticalAlign: 'middle',
+    borderBottom: [3, "solid", chroma(colors.primary).darken(0.4).hex()],
+    verticalAlign: "middle",
     lineHeight: 2,
-    fontSize: '0.8em',
+    fontSize: "0.8em",
     fontWeight: 600,
   },
 
   headerLink: {
-    cursor: 'pointer',
+    cursor: "pointer",
     color: colors.white,
-    display: 'inline-block',
+    display: "inline-block",
     padding: [2, 8],
 
-    '&:hover': {
+    "&:hover": {
       color: colors.white,
-      textDecoration: 'none',
+      textDecoration: "none",
 
       background: chroma(colors.primary).darken(0.4).hex(),
       borderRadius: 6,
@@ -47,7 +47,7 @@ const styles: JSSStyles = {
   },
 
   activeLink: {
-    textDecoration: 'underline',
+    textDecoration: "underline",
   },
 
   saveNotification: {
@@ -57,31 +57,29 @@ const styles: JSSStyles = {
 
   headerDropdown: {
     marginRight: 10,
-    display: 'inline',
+    display: "inline",
   },
 
   headerLogo: {
     flex: 1,
-    fontSize: '1rem',
+    fontSize: "1rem",
   },
 
   version: {
     color: chroma.mix(colors.white, colors.primary, 0.3).hex(),
     marginLeft: 5,
-    fontSize: '0.8em',
+    fontSize: "0.8em",
   },
 
   headerLogoLink: {
     marginRight: 15,
   },
 
-  headerContent: {
-
-  },
+  headerContent: {},
 
   headerUserProfile: {
-    '& .dropdown-menu.dropdown-menu-right': {
-      textAlign: 'center',
+    "& .dropdown-menu.dropdown-menu-right": {
+      textAlign: "center",
     },
     headerLink: {
       marginRight: 0,
@@ -103,70 +101,74 @@ export interface HeaderState {
 }
 
 type LinkProps = {
-  className?: string,
-  action: any,
-  children: any,
+  className?: string;
+  action: any;
+  children: any;
 };
 
 /**
  * Link React Component
  */
-const Link = withStyles<LinkProps>(styles)(({
-  className,
-  action,
-  children,
-  classes,
-}) => (
-    <a className={classNames([classes.headerLink, className])} href="#"
-      onClick={(e) => { e.preventDefault(); action(); }}>{children}</a>
-  ));
+const Link = withStyles<LinkProps>(styles)(
+  ({ className, action, children, classes }) => (
+    <a
+      className={classNames([classes.headerLink, className])}
+      href="#"
+      onClick={(e) => {
+        e.preventDefault();
+        action();
+      }}
+    >
+      {children}
+    </a>
+  )
+);
 
 type MenuProps = {
-  label: string | JSX.Element,
-  children: any,
+  label: string | JSX.Element;
+  children: any;
 };
 
 /**
  * Menu React Component
  */
-const Menu = withStyles<MenuProps>(styles)(({
-  label,
-  children,
-  classes,
-}) => (
-    <div className={classNames([classes.headerDropdown, 'dropdown show'])}>
-      <a className={classNames([classes.headerLink, 'dropdown-toggle'])} href="#"
-        target="_blank"
-        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {label}
-      </a>
-      <div className="dropdown-menu dropdown-menu-right">
-        {children}
-      </div>
-    </div>
-  ));
+const Menu = withStyles<MenuProps>(styles)(({ label, children, classes }) => (
+  <div className={classNames([classes.headerDropdown, "dropdown show"])}>
+    <a
+      className={classNames([classes.headerLink, "dropdown-toggle"])}
+      href="#"
+      target="_blank"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      {label}
+    </a>
+    <div className="dropdown-menu dropdown-menu-right">{children}</div>
+  </div>
+));
 
 type MenuItemProps = {
-  url: string,
-  children: any,
+  url: string;
+  children: any;
 };
 
 /**
  * MenuItem React Component
  */
-const MenuItem = withStyles<MenuItemProps>(styles)(({
-  url,
-  children,
-}) => (
-    <a className="dropdown-item" href={url}>{children}</a>
-  ));
+const MenuItem = withStyles<MenuItemProps>(styles)(({ url, children }) => (
+  <a className="dropdown-item" href={url}>
+    {children}
+  </a>
+));
 
 /**
  * Header React Component
  */
-class Header
-  extends React.PureComponent<StyledComponentProps<HeaderProps, typeof styles>, HeaderState> {
-
+class Header extends React.PureComponent<
+  StyledComponentProps<HeaderProps, typeof styles>,
+  HeaderState
+> {
   timer: any;
 
   constructor(props) {
@@ -182,8 +184,8 @@ class Header
     const name = user.profile.username;
     const email = user.profile.email;
     const formUrl = buildFeedbackFromCurrent(name, email);
-    const QUICK_START_GUIDE_URL
-      = 'https://olihelp.freshdesk.com/support/solutions/32000022976';
+    const QUICK_START_GUIDE_URL =
+      "https://olihelp.freshdesk.com/support/solutions/32000022976";
     const logoutUrl = user.logoutUrl;
 
     return (
@@ -191,12 +193,8 @@ class Header
         <Menu label={user.profile.username}>
           <h6 className="dropdown-header">Course Author v{getVersion()}</h6>
           <div className="dropdown-divider"></div>
-          <MenuItem url={QUICK_START_GUIDE_URL}>
-            Quick Start Guide
-          </MenuItem>
-          <MenuItem url={formUrl}>
-            Help / Feedback
-          </MenuItem>
+          <MenuItem url={QUICK_START_GUIDE_URL}>Quick Start Guide</MenuItem>
+          <MenuItem url={formUrl}>Help / Feedback</MenuItem>
           <div className="dropdown-divider"></div>
           <MenuItem url={logoutUrl}>Log Out</MenuItem>
         </Menu>
@@ -205,19 +203,24 @@ class Header
   }
 
   componentWillReceiveProps(nextProps: HeaderProps) {
-
-    if (nextProps.saveCount > this.props.saveCount
-      && nextProps.isSaveInProcess
-      && nextProps.isSaveInProcess === this.props.isSaveInProcess) {
-
+    if (
+      nextProps.saveCount > this.props.saveCount &&
+      nextProps.isSaveInProcess &&
+      nextProps.isSaveInProcess === this.props.isSaveInProcess
+    ) {
       this.setState({ showIncrementalSave: true });
 
-      this.timer = setTimeout(() => this.setState({ showIncrementalSave: false }), 1000);
+      this.timer = setTimeout(
+        () => this.setState({ showIncrementalSave: false }),
+        1000
+      );
     }
 
-    if (nextProps.isSaveInProcess && !this.props.isSaveInProcess
-      && this.state.showIncrementalSave) {
-
+    if (
+      nextProps.isSaveInProcess &&
+      !this.props.isSaveInProcess &&
+      this.state.showIncrementalSave
+    ) {
       this.setState({ showIncrementalSave: false });
       if (this.timer !== null) {
         clearTimeout(this.timer);
@@ -229,8 +232,10 @@ class Header
   renderSaveNotification() {
     const { classes } = this.props;
 
-    const lastSucceeded
-      = this.props.lastRequestSucceeded.caseOf({ just: v => v, nothing: () => false });
+    const lastSucceeded = this.props.lastRequestSucceeded.caseOf({
+      just: (v) => v,
+      nothing: () => false,
+    });
 
     if (this.props.isSaveInProcess && this.state.showIncrementalSave) {
       return <div className={classes.saveNotification}>Saved</div>;
@@ -247,32 +252,40 @@ class Header
   renderPackageTitle() {
     const { classes, course, router } = this.props;
 
-    const active = router.route.type === 'RouteCourse'
-      && router.route.route.type === 'RouteCourseOverview';
+    const active =
+      router.route.type === "RouteCourse" &&
+      router.route.route.type === "RouteCourseOverview";
 
-    const orgId = router.route.type === 'RouteCourse'
-      ? router.route.orgId
-      : Maybe.nothing<string>();
+    const orgId =
+      router.route.type === "RouteCourse"
+        ? router.route.orgId
+        : Maybe.nothing<string>();
 
     return (
       <span>
-        <a className={classNames([classes.headerLink, active ? classes.activeLink : ''])}
+        <a
+          className={classNames([
+            classes.headerLink,
+            active ? classes.activeLink : "",
+          ])}
           onClick={(e) => {
             e.preventDefault();
             viewActions.viewCourse(course.idvers, orgId);
-          }}>
+          }}
+        >
           {course.title}
-        </a> <span className={classes.version}>v{course.version}</span>
+        </a>{" "}
+        <span className={classes.version}>v{course.version}</span>
       </span>
     );
   }
 
   renderApplicationLabel() {
-    return <span>OLI Course Authoring</span>;
+    return <span>OLI Echo</span>;
   }
 
   isCourseRoute() {
-    return this.props.router.route.type === 'RouteCourse';
+    return this.props.router.route.type === "RouteCourse";
   }
 
   render() {
@@ -281,35 +294,43 @@ class Header
     // For our purposes, we need to know if the current route is either course-specific
     // or application specific.  If course specific, we display the title of the package
     // here in the header, otherwise for all other routes we display the application title
-    const label = this.isCourseRoute() && this.props.course
-    ? this.renderPackageTitle() : this.renderApplicationLabel();
+    const label =
+      this.isCourseRoute() && this.props.course
+        ? this.renderPackageTitle()
+        : this.renderApplicationLabel();
 
     return (
-      <div className={classNames(['Header', classes.Header])}>
+      <div className={classNames(["Header", classes.Header])}>
         <div className={classes.headerLogo}>
-          <Link className={classes.headerLogoLink} action={viewActions.viewAllCourses}>
-            {router.route.type !== 'RouteRoot'
-              ? <i className="fa fa-chevron-left" style={{ marginRight: 10 }} />
-              : undefined
-            }
-            <img src={OLI_ICON} width="30" height="30"
-              className="d-inline-block align-top" alt="" />
+          <Link
+            className={classes.headerLogoLink}
+            action={viewActions.viewAllCourses}
+          >
+            {router.route.type !== "RouteRoot" ? (
+              <i className="fa fa-chevron-left" style={{ marginRight: 10 }} />
+            ) : undefined}
+            <img
+              src={OLI_ICON}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
           </Link>
 
           {label}
-
         </div>
-        <ReactCSSTransitionGroup transitionName="saving"
-          transitionEnterTimeout={250} transitionLeaveTimeout={500}>
+        <ReactCSSTransitionGroup
+          transitionName="saving"
+          transitionEnterTimeout={250}
+          transitionLeaveTimeout={500}
+        >
           {this.renderSaveNotification()}
         </ReactCSSTransitionGroup>
-        <div className={classes.headerUserProfile}>
-          {this.renderAbout()}
-        </div>
+        <div className={classes.headerUserProfile}>{this.renderAbout()}</div>
       </div>
     );
   }
-
 }
 
 const StyledHeader = withStyles<HeaderProps>(styles)(Header);
